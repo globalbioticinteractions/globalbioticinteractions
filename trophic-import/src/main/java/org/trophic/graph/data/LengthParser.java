0555c6cfce8076af48bc01fed859ac361da2e13a
reiz <robert.reiz@gmx.com>
2011-10-19 20:25:46 -0700

    remove some files

--- trophic-import/src/main/java/org/trophic/graph/data/LengthParser.java
+++
@@ -1,7 +0,0 @@
-package org.trophic.graph.data;
-
-import com.Ostermiller.util.LabeledCSVParser;
-
-public interface LengthParser {
-    Double parseLengthInMm(LabeledCSVParser parser) throws StudyImporterException;
-}
--- trophic-movies/src/main/java/org/trophic/graph/domain/Roles.java
+++
@@ -1,9 +0,0 @@
-package org.trophic.graph.domain;
-
-/**
-* @author mh
-* @since 12.03.11
-*/
-public enum Roles {
-    ACTS_IN, DIRECTED
-}
