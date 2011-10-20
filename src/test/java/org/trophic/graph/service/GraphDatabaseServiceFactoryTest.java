36b1c97d370c962644454de07178afd04958302c
Jorrit Poelen <jhpoelen@gmail.com>
2011-10-19 23:02:52 -0700

    added test for "test" neo4j instance

--- src/test/java/org/trophic/graph/service/GraphDatabaseServiceFactoryTest.java
+++ src/test/java/org/trophic/graph/service/GraphDatabaseServiceFactoryTest.java
@@ -4,11 +4,26 @@ import org.junit.Test;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.kernel.EmbeddedGraphDatabase;
 
+import java.io.IOException;
+
 public class GraphDatabaseServiceFactoryTest {
 
 
     @Test
-    public void getInstance() {
+    public void getTestInstance() throws IOException {
+        final GraphDatabaseService graphDb = new org.neo4j.test.ImpermanentGraphDatabase();
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                graphDb.shutdown();
+            }
+        });
+        graphDb.shutdown();
+
+    }
+
+    @Test
+    public void getInstance() throws IOException {
         final GraphDatabaseService graphDb = new EmbeddedGraphDatabase("data");
         Runtime.getRuntime().addShutdownHook(new Thread() {
             @Override
