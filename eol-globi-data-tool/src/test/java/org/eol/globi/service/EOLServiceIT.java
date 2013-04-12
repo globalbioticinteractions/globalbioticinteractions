36353701f9e92423eb0da402dce85b1d47d9dee1
Jorrit Poelen <jhpoelen@gmail.com>
2013-04-12 14:52:04 -0700

    refactor test case for readability

--- eol-globi-data-tool/src/test/java/org/eol/globi/service/EOLServiceIT.java
+++ eol-globi-data-tool/src/test/java/org/eol/globi/service/EOLServiceIT.java
@@ -17,9 +17,7 @@ public class EOLServiceIT {
         assertThat(lookupPageIdByScientificName("Dead roots"), is("EOL:19665069"));
         assertThat(lookupPageIdByScientificName("Prunella (Bot)"), is("EOL:70879"));
         assertThat(lookupPageIdByScientificName("Prunella (Bird)"), is("EOL:77930"));
-        assertThat(lookupPageIdByScientificName("Salix cinerea x phylicifolia"), is("EOL:584272"));
-
-
+        assertThat(lookupPageIdByScientificName("Pseudobaeospora dichroa"), is("EOL:584272"));
     }
 
     /** Regarding multiple symbol in taxonomic names:
@@ -44,6 +42,7 @@ public class EOLServiceIT {
 
     @Test
     public void multiplySymbolNotSupportedByEOL() throws TaxonPropertyLookupServiceException {
+        assertThat(lookupPageIdByScientificName("Salix cinerea x phylicifolia"), is("EOL:584272"));
         assertThat(lookupPageIdByScientificName("Salix cinerea \u00D7 phylicifolia"), is(nullValue()));
         assertThat(lookupPageIdByScientificName("Salix cinerea × phylicifolia"), is(nullValue()));
 
