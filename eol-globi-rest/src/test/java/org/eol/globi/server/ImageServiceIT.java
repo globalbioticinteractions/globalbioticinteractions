0230870275e852b38655546196e6a61d18657336
Jorrit Poelen <jhpoelen@gmail.com>
2013-04-21 22:46:16 -0700

    unit test fix

--- eol-globi-rest/src/test/java/org/eol/globi/server/ImageServiceIT.java
+++ eol-globi-rest/src/test/java/org/eol/globi/server/ImageServiceIT.java
@@ -13,7 +13,7 @@ public class ImageServiceIT extends ITBase {
     public void findImagesForExternalId() throws IOException {
         String uri = getURLPrefix() + "images/EOL:276287";
         String response = HttpClient.httpGet(uri);
-        assertThat(response, is("{\"\"thumbnailURL\":\"http://media.eol.org/content/2011/12/13/21/66989_98_68.jpg\",\"imageURL\":\"http://media.eol.org/content/2011/12/13/21/66989_orig.jpg\",\"eolpageId\":\"276287\"}"));
+        assertThat(response, is("{\"thumbnailURL\":\"http://media.eol.org/content/2011/12/13/21/66989_98_68.jpg\",\"imageURL\":\"http://media.eol.org/content/2011/12/13/21/66989_orig.jpg\",\"eolpageId\":\"276287\"}"));
     }
 
 }
