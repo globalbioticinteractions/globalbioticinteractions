934087877e7117a74c02a171a5f79049a5af6e9b
Jorrit Poelen <jhpoelen@gmail.com>
2013-04-29 15:04:09 -0700

    fix issue reported by jim where predator items in gomexsi only had single prey item

--- eol-globi-data-tool/src/main/java/org/eol/globi/data/StudyImporterForGoMexSI.java
+++ eol-globi-data-tool/src/main/java/org/eol/globi/data/StudyImporterForGoMexSI.java
@@ -8,7 +8,9 @@ import org.eol.globi.domain.Specimen;
 import org.eol.globi.domain.Study;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class StudyImporterForGoMexSI extends BaseStudyImporter {
@@ -27,18 +29,23 @@ public class StudyImporterForGoMexSI extends BaseStudyImporter {
                 "",
                 "<a href=\"http://www.ingentaconnect.com/content/umrsmas/bullmar/2013/00000089/00000001/art00009\">Building a Fisheries Trophic Interaction Database for Management and Modeling Research in the Gulf of Mexico Large Marine Ecosystem.</a>");
 
-        Map<String, Specimen> predatorIdToPredatorSpecimen = new HashMap<String, Specimen>();
-        Map<String, Specimen> predatorIdToPreySpecimen = new HashMap<String, Specimen>();
+        Map<String, List<Specimen>> predatorIdToPredatorSpecimen = new HashMap<String, List<Specimen>>();
+        Map<String, List<Specimen>> predatorIdToPreySpecimen = new HashMap<String, List<Specimen>>();
         addSpecimen(predatorIdToPredatorSpecimen, "gomexsi/Predators.csv", "PRED_SCI_NAME");
         addSpecimen(predatorIdToPreySpecimen, "gomexsi/Prey.csv", "DATABASE_PREY_NAME");
 
-        for (Map.Entry<String, Specimen> predatorIdPredatorSpecimen : predatorIdToPredatorSpecimen.entrySet()) {
-            Specimen predatorSpecimen = predatorIdPredatorSpecimen.getValue();
-            study.collected(predatorSpecimen);
-            Specimen specimen = predatorIdToPreySpecimen.get(predatorIdPredatorSpecimen.getKey());
-            if (specimen != null) {
-                predatorSpecimen.ate(specimen);
+        for (Map.Entry<String, List<Specimen>> predatorIdPredatorSpecimen : predatorIdToPredatorSpecimen.entrySet()) {
+            List<Specimen> predatorSpecimens = predatorIdPredatorSpecimen.getValue();
+            for (Specimen predatorSpecimen : predatorSpecimens) {
+                study.collected(predatorSpecimen);
+                List<Specimen> preySpecimens = predatorIdToPreySpecimen.get(predatorIdPredatorSpecimen.getKey());
+                if (preySpecimens != null) {
+                    for (Specimen preySpecimen : preySpecimens) {
+                        predatorSpecimen.ate(preySpecimen);
+                    }
+                }
             }
+
         }
 
         addLocations(predatorIdToPredatorSpecimen);
@@ -46,7 +53,7 @@ public class StudyImporterForGoMexSI extends BaseStudyImporter {
         return study;
     }
 
-    private void addLocations(Map<String, Specimen> predatorIdToPredatorSpecimen) throws StudyImporterException {
+    private void addLocations(Map<String, List<Specimen>> predatorIdToPredatorSpecimen) throws StudyImporterException {
         String locationResource = "gomexsi/Locations.csv";
         try {
             LabeledCSVParser parser = parserFactory.createParser(locationResource, CharsetConstant.UTF8);
@@ -57,11 +64,13 @@ public class StudyImporterForGoMexSI extends BaseStudyImporter {
                 Double longitude = getMandatoryDoubleValue(locationResource, parser, "LOC_CENTR_LONG");
                 Double depth = getMandatoryDoubleValue(locationResource, parser, "MN_DEP_SAMP");
                 Location location = nodeFactory.getOrCreateLocation(latitude, longitude, depth == null ? null : -depth);
-                Specimen specimen = predatorIdToPredatorSpecimen.get(refId + specimenId);
-                if (specimen == null) {
+                List<Specimen> specimens = predatorIdToPredatorSpecimen.get(refId + specimenId);
+                if (specimens == null) {
                     LOG.warn("failed to lookup location for predator [" + refId + ":" + specimenId + "] from [" + locationResource + ":" + parser.getLastLineNumber() + "]");
                 } else {
-                    specimen.caughtIn(location);
+                    for (Specimen specimen : specimens) {
+                        specimen.caughtIn(location);
+                    }
                 }
             }
         } catch (IOException e) {
@@ -78,7 +87,7 @@ public class StudyImporterForGoMexSI extends BaseStudyImporter {
         }
     }
 
-    private void addSpecimen(Map<String, Specimen> specimenMap, String datafile, String scientificNameLabel) throws StudyImporterException {
+    private void addSpecimen(Map<String, List<Specimen>> specimenMap, String datafile, String scientificNameLabel) throws StudyImporterException {
         try {
             LabeledCSVParser parser = parserFactory.createParser(datafile, CharsetConstant.UTF8);
             while (parser.getLine() != null) {
@@ -86,7 +95,13 @@ public class StudyImporterForGoMexSI extends BaseStudyImporter {
                 String specimenId = getMandatoryValue(datafile, parser, "PRED_ID");
                 String scientificName = getMandatoryValue(datafile, parser, scientificNameLabel);
                 Specimen specimen = nodeFactory.createSpecimen(scientificName);
-                specimenMap.put(refId + specimenId, specimen);
+                String predatorUID = refId + specimenId;
+                List<Specimen> specimenList = specimenMap.get(predatorUID);
+                if (specimenList == null) {
+                    specimenList = new ArrayList<Specimen>();
+                }
+                specimenList.add(specimen);
+                specimenMap.put(predatorUID, specimenList);
             }
         } catch (IOException e) {
             throw new StudyImporterException("failed to open resource [" + datafile + "]", e);
--- eol-globi-data-tool/src/test/java/org/eol/globi/data/StudyImporterForGoMexSITest.java
+++ eol-globi-data-tool/src/test/java/org/eol/globi/data/StudyImporterForGoMexSITest.java
@@ -1,9 +1,19 @@
 package org.eol.globi.data;
 
 
+import org.eol.globi.domain.RelTypes;
+import org.eol.globi.domain.Specimen;
+import org.eol.globi.domain.Taxon;
 import org.junit.Test;
+import org.neo4j.graphdb.Direction;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+
+import java.util.ArrayList;
 
 import static junit.framework.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.hasItem;
 
 public class StudyImporterForGoMexSITest extends GraphDBTestCase {
 
@@ -18,6 +28,24 @@ public class StudyImporterForGoMexSITest extends GraphDBTestCase {
 
         assertNotNull(nodeFactory.findTaxonOfType("Amphipoda"));
         assertNotNull(nodeFactory.findTaxonOfType("Crustacea"));
+
+        Taxon taxon = nodeFactory.findTaxonOfType("Scomberomorus cavalla");
+        ArrayList<String> preyList = new ArrayList<String>();
+        Iterable<Relationship> classifiedAsRels = taxon.getUnderlyingNode().getRelationships(Direction.INCOMING, RelTypes.CLASSIFIED_AS);
+        for (Relationship classifiedAsRel : classifiedAsRels) {
+            Node specimen = classifiedAsRel.getStartNode();
+            Specimen specimen1 = new Specimen(specimen);
+            Iterable<Relationship> stomachContents = specimen1.getStomachContents();
+            for (Relationship prey : stomachContents) {
+                Relationship singleRelationship = prey.getEndNode().getSingleRelationship(RelTypes.CLASSIFIED_AS, Direction.OUTGOING);
+                preyList.add((String) singleRelationship.getEndNode().getProperty("name"));
+            }
+        }
+        assertThat(preyList, hasItem("Organic matter"));
+        assertThat(preyList, hasItem("Triglidae"));
+        assertThat(preyList, hasItem("Sparidae"));
+
+        assertNotNull(taxon);
 
         assertNotNull(nodeFactory.findLocation(29.346953, -92.980614, -13.641));
 
