b39af4821052e79a9ef0ed6a796880e68e5fa585
Jorrit Poelen <jhpoelen@gmail.com>
2013-07-25 11:55:13 -0700

    added first test case for using owlapi to read globi.owl ontology

--- eol-globi-data-tool/src/test/java/org/eol/globi/export/RDFExporterTest.java
+++ eol-globi-data-tool/src/test/java/org/eol/globi/export/RDFExporterTest.java
@@ -1,19 +1,58 @@
 package org.eol.globi.export;
 
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.rdf.model.Resource;
-import org.junit.Test;
-import org.neo4j.graphdb.Relationship;
+import org.apache.commons.io.IOUtils;
 import org.eol.globi.data.GraphDBTestCase;
 import org.eol.globi.data.NodeFactoryException;
 import org.eol.globi.domain.InteractType;
 import org.eol.globi.domain.Location;
 import org.eol.globi.domain.Specimen;
 import org.eol.globi.domain.Study;
-import org.eol.globi.domain.Taxon;
+import org.junit.Test;
+import org.neo4j.graphdb.Relationship;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.io.OWLOntologyDocumentSource;
+import org.semanticweb.owlapi.io.StringDocumentSource;
+import org.semanticweb.owlapi.io.StringDocumentTarget;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.OWLOntologyStorageException;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.junit.internal.matchers.StringContains.containsString;
 
 public class RDFExporterTest extends GraphDBTestCase {
+
+    @Test
+    public void readGloBIOntology() throws OWLOntologyCreationException, OWLOntologyStorageException, IOException {
+        InputStream resourceAsStream = getClass().getResourceAsStream("/globi.owl");
+        assertThat(resourceAsStream, is(notNullValue()));
+
+        OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        IOUtils.copy(resourceAsStream, baos);
+
+
+        String sourceOntologyString = baos.toString("UTF-8");
+        OWLOntologyDocumentSource source = new StringDocumentSource(sourceOntologyString);
+
+        assertThat(sourceOntologyString, containsString("Prefix"));
+        OWLOntology globi = manager.loadOntologyFromOntologyDocument(source);
+
+        assertThat(globi, is(notNullValue()));
+
+        StringDocumentTarget target = new StringDocumentTarget();
+        manager.saveOntology(globi, target);
+
+        assertThat(target.toString(), containsString("Prefix"));
+    }
 
     @Test
     public void exportToRDF() throws NodeFactoryException {
@@ -31,50 +70,6 @@ public class RDFExporterTest extends GraphDBTestCase {
         Relationship preysUponRelationship = man.createRelationshipTo(dog, InteractType.ATE);
 
 
-        // some definitions
-
-        String rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
-
-
-        String entityPrefix = "http://tinyurl.com/9dqkp2m/";
-
-        String geoNS = "http://www.w3.org/2003/01/geo/wgs84_pos#";
-
-        // create an empty Model
-        Model model = ModelFactory.createDefaultModel();
-        String trank = "http://rs.tdwg.org/ontology/voc/TaxonRank#";
-        model.setNsPrefix("trank", trank);
-
-        String tn = "http://rs.tdwg.org/ontology/voc/TaxonName#";
-        model.setNsPrefix("tn", tn);
-        String tinter = "http://rs.tdwg.org/ontology/voc/TaxonOccurrenceInteraction#";
-        model.setNsPrefix("tinter", tinter);
-
-        // create the resource
-        //   and add the properties cascading style
-        Resource specimenResource = model.createResource("http://rs.tdwg.org/ontology/voc/Specimen");
-
-        Resource dogSpecimen = model.createResource(entityPrefix + dog.getNodeID())
-                .addProperty(model.createProperty(tn + "rank"), model.createResource(tn + "Species"))
-                .addProperty(model.createProperty(tn + "nameComplete"), "Canis lupus")
-                .addProperty(model.createProperty(rdf + "type"), specimenResource);
-
-
-        Resource interaction = model.createResource(entityPrefix + preysUponRelationship.getId());
-
-        Resource manSpecimen = model.createResource(entityPrefix + man.getNodeID())
-                .addProperty(model.createProperty(tn + "rank"), model.createResource(tn + "Species"))
-                .addProperty(model.createProperty(tn + "nameComplete"), "Homo sapiens")
-                .addProperty(model.createProperty(rdf + "type"), specimenResource);
-
-        interaction
-                .addProperty(model.createProperty(rdf + "type"), model.createResource("http://rs.tdwg.org/ontology/voc/TaxonOccurrenceInteraction"))
-                .addProperty(model.createProperty(tinter + "fromOccurance"), manSpecimen)
-                .addProperty(model.createProperty(tinter + "interactionCategory"), model.createResource("http://rs.tdwg.org/ontology/voc/TaxonOccurrenceInteraction#PreysUpon"))
-                .addProperty(model.createProperty(tinter + "toOccurance"), dogSpecimen);
-
-        // now write the model in XML form to a file
-        model.write(System.out);
 
     }
 }
