5fb96d1685ed7be5ad7e3341c397763b8d595adf
Jorrit Poelen <jhpoelen@gmail.com>
2013-07-29 13:09:27 -0700

    removed re-use of property maps

--- eol-globi-data-tool/src/main/java/org/eol/globi/export/ExporterOccurrenceAggregates.java
+++ eol-globi-data-tool/src/main/java/org/eol/globi/export/ExporterOccurrenceAggregates.java
@@ -17,23 +17,25 @@ public class ExporterOccurrenceAggregates extends ExporterOccurrencesBase {
     public void doExportStudy(Study study, Writer writer, boolean includeHeader) throws IOException {
         ExecutionEngine engine = new ExecutionEngine(study.getUnderlyingNode().getGraphDatabase());
         ExecutionResult results = engine.execute(getQueryForDistinctTargetTaxaForPreyBySourceTaxa(study));
-        Map<String, String> properties = new HashMap<String, String>();
+
         for (Map<String, Object> result : results) {
-            populateRow(study, writer, properties, result);
+            populateRow(study, writer, result);
         }
     }
 
-    private void populateRow(Study study, Writer writer, Map<String, String> properties, Map<String, Object> result) throws IOException {
+    private void populateRow(Study study, Writer writer, Map<String, Object> result) throws IOException {
+        Map<String, String> sourceProperties = new HashMap<String, String>();
         Node sourceTaxon = (Node) result.get(QUERY_PARAM_SOURCE_TAXON);
         String relationshipType = (String) result.get(QUERY_PARAM_INTERACTION_TYPE);
 
         String sourceOccurrenceId = study.getUnderlyingNode().getId() + "-" + sourceTaxon.getId() + "-" + relationshipType;
-        writeRow(study, writer, properties, sourceTaxon, "globi:occur:source:" + sourceOccurrenceId);
+        writeRow(study, writer, sourceProperties, sourceTaxon, "globi:occur:source:" + sourceOccurrenceId);
 
         JavaConversions.SeqWrapper<Node> targetTaxa = (JavaConversions.SeqWrapper<Node>) result.get(QUERY_PARAM_TARGET_TAXA);
         for (Node targetTaxon : targetTaxa) {
+            Map<String, String> targetProperties = new HashMap<String, String>();
             String targetOccurrenceId = sourceOccurrenceId + "-" + targetTaxon.getId();
-            writeRow(study, writer, properties, targetTaxon, "globi:occur:target:" + targetOccurrenceId);
+            writeRow(study, writer, targetProperties, targetTaxon, "globi:occur:target:" + targetOccurrenceId);
         }
     }
 
