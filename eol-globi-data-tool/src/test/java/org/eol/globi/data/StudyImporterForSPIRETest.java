09b3806d8e2380419f036cb0aa27343ff45965e4
Jorrit Poelen <jhpoelen@gmail.com>
2013-08-05 17:00:27 -0700

    gathered some stats from MDB SPIRE import

--- eol-globi-data-tool/src/test/java/org/eol/globi/data/StudyImporterForSPIRETest.java
+++ eol-globi-data-tool/src/test/java/org/eol/globi/data/StudyImporterForSPIRETest.java
@@ -6,14 +6,16 @@ import com.healthmarketscience.jackcess.JetFormat;
 import com.healthmarketscience.jackcess.Table;
 import com.hp.hpl.jena.rdf.model.impl.RDFDefaultErrorHandler;
 import org.apache.commons.collections.CollectionUtils;
-import org.hamcrest.core.Is;
-import org.junit.Test;
 import org.eol.globi.domain.Study;
+import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -34,7 +36,7 @@ public class StudyImporterForSPIRETest extends GraphDBTestCase {
         Database db = Database.open(new File(uri), true);
         assertThat(db.getFileFormat().getJetFormat(), is(JetFormat.VERSION_4));
 
-        String[] tableNames = new String[] {
+        String[] tableNames = new String[]{
                 "attribute_types",
                 "common_names",
                 "entities",
@@ -52,19 +54,50 @@ public class StudyImporterForSPIRETest extends GraphDBTestCase {
 
         };
         Set<String> expectedSet = new HashSet<String>();
-        for (String tableName : tableNames) {
-            expectedSet.add(tableName);
-        }
+        Collections.addAll(expectedSet, tableNames);
 
         Set<String> actualTableNames = db.getTableNames();
         assertThat(actualTableNames.size(), is(not(0)));
-        assertThat("expected tables names [" + tableNames + "] to be present",
+        assertThat("expected tables names [" + Arrays.toString(tableNames) + "] to be present",
                 CollectionUtils.subtract(expectedSet, actualTableNames).size(), is(0));
 
         Table studies = db.getTable("studies");
         for (Map<String, Object> study : studies) {
             assertNotNull(study.get("reference"));
         }
+
+        List<String> expectedColumnNames = Arrays.asList("study_id", "entity1", "entity2", "link_strength", "link_type", "table_ref", "link_number");
+        assertColumnNames(expectedColumnNames, db.getTable("links"));
+
+        expectedColumnNames = Arrays.asList("id", "latinname", "commonname", "parent", "webinfo", "moreinfo", "numchildren", "pos", "classification", "pictures", "sounds", "specimens", "idx", "extinct", "rank");
+        assertColumnNames(expectedColumnNames, db.getTable("taxon"));
+
+        Table taxonTable = db.getTable("taxon");
+        int numberOfTaxa = 0;
+        while (taxonTable.getNextRow() != null) {
+            numberOfTaxa++;
+        }
+
+        assertThat(numberOfTaxa, is(198301));
+
+        Table links = db.getTable("links");
+        int numberOfLinks = 0;
+        while (links.getNextRow() != null) {
+            numberOfLinks++;
+        }
+
+        assertThat(numberOfLinks, is(18189));
+    }
+
+    private void assertColumnNames(List<String> expectedColumnNames, Table table) throws IOException {
+        Table links = table;
+        List<String> actualColumnNames = new ArrayList<String>();
+        List<Column> columns = links.getColumns();
+        for (Column column : columns) {
+            actualColumnNames.add(column.getName());
+        }
+
+        assertThat(actualColumnNames, is(expectedColumnNames));
     }
 
     @Test
