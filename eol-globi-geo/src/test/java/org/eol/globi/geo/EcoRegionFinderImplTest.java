e5b0adbe56a4dfe3981af02bac08bb929c50c962
Jorrit Poelen <jhpoelen@gmail.com>
2014-01-20 08:40:44 -0800

    rename test to align with class under test

--- eol-globi-geo/src/test/java/org/eol/globi/geo/EcoRegionFinderImplTest.java
+++ eol-globi-geo/src/test/java/org/eol/globi/geo/EcoRegionFinderImplTest.java
@@ -11,7 +11,7 @@ import static junit.framework.Assert.assertNull;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-public class EcoRegionsServiceTest {
+public class EcoRegionFinderImplTest {
 
     @Test
     public void fourDifferentEcoRegionsGulfOfMexico() throws EcoRegionFinderException {
