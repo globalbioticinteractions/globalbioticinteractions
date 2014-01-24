deab159761d235dc40b45c37ba3874a919d3251f
Jorrit Poelen <jhpoelen@gmail.com>
2014-01-23 16:56:49 -0800

    add underscores to column names for readability

--- eol-globi-rest/src/main/java/org/eol/globi/server/CypherProxyController.java
+++ eol-globi-rest/src/main/java/org/eol/globi/server/CypherProxyController.java
@@ -205,9 +205,9 @@ public class CypherProxyController {
                 .append("targetSpecimen." + Specimen.BODY_PART_LABEL + "? as ").append(preyPrefix).append(ResultFields.SUFFIX_BODY_PART).append(",")
                 .append("sourceSpecimen." + Specimen.PHYSIOLOGICAL_STATE_LABEL + "? as ").append(predatorPrefix).append(ResultFields.SUFFIX_PHYSIOLOGICAL_STATE).append(",")
                 .append("targetSpecimen." + Specimen.PHYSIOLOGICAL_STATE_LABEL + "? as ").append(preyPrefix).append(ResultFields.SUFFIX_PHYSIOLOGICAL_STATE).append(",")
-                .append("targetSpecimen." + Specimen.TOTAL_COUNT + "? as ").append(preyPrefix).append("total_count").append(",")
-                .append("targetSpecimen." + Specimen.TOTAL_VOLUME_IN_ML + "? as ").append(preyPrefix).append("total_volume_ml").append(",")
-                .append("targetSpecimen." + Specimen.FREQUENCY_OF_OCCURRENCE + "? as ").append(preyPrefix).append("frequency_of_occurrence");
+                .append("targetSpecimen." + Specimen.TOTAL_COUNT + "? as ").append(preyPrefix).append("_total_count").append(",")
+                .append("targetSpecimen." + Specimen.TOTAL_VOLUME_IN_ML + "? as ").append(preyPrefix).append("_total_volume_ml").append(",")
+                .append("targetSpecimen." + Specimen.FREQUENCY_OF_OCCURRENCE + "? as ").append(preyPrefix).append("_frequency_of_occurrence");
 
         if (INTERACTION_PREYS_ON.equals(interactionType)) {
             query.append("START ").append(getTaxonSelector(sourceTaxonName, targetTaxonName)).append(" ")
