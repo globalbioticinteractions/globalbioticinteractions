143d8d51e002b45a8f0320a932125ab30c011f2b
Jorrit Poelen <jhpoelen@gmail.com>
2014-02-20 14:17:39 -0800

    tmp fix

--- eol-globi-data-tool/src/main/java/org/eol/globi/data/StudyImporterFactory.java
+++ eol-globi-data-tool/src/main/java/org/eol/globi/data/StudyImporterFactory.java
@@ -8,7 +8,7 @@ import java.util.Collections;
 public class StudyImporterFactory {
 
     private static final Collection<Class> IMPORTERS = Collections.unmodifiableCollection(new ArrayList<Class>() {{
-        add(StudyImporterForThessen.class);
+        //add(StudyImporterForThessen.class);
         add(StudyImporterForBioInfo.class);
         add(StudyImporterForRaymond.class);
         add(StudyImporterForLifeWatchGreece.class);
