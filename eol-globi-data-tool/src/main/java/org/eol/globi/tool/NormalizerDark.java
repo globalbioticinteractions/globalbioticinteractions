7dc8dea41ef0ccddadf8d6f6daf7d287acb0c0bf
Jorrit Poelen <jhpoelen@gmail.com>
2014-05-05 08:59:05 -0700

    introducing dark globi module

--- eol-globi-data-tool/src/main/java/org/eol/globi/tool/NormalizerDark.java
+++ eol-globi-data-tool/src/main/java/org/eol/globi/tool/NormalizerDark.java
@@ -2,6 +2,7 @@ package org.eol.globi.tool;
 
 import org.eol.globi.data.StudyImporterException;
 import org.eol.globi.data.StudyImporterFactory;
+import org.eol.globi.data.StudyImporterForFWDP;
 import org.eol.globi.data.StudyImporterForSAID;
 
 import java.util.ArrayList;
@@ -11,7 +12,7 @@ public class NormalizerDark {
 
     public static void main(final String[] commandLineArguments) throws StudyImporterException {
         new Normalizer().normalize(new ArrayList<Class>() {{
-            add(StudyImporterForSAID.class);
+            add(StudyImporterForFWDP.class);
         }});
     }
 
