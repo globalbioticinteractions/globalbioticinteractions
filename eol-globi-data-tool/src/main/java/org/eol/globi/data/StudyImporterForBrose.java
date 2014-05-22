d908217463b67ce895ce5638272cfb0040e00f5f
Jorrit Poelen <jhpoelen@gmail.com>
2014-05-22 12:40:33 -0700

    add first pass at Brose et al. 2005

---
+++ eol-globi-data-tool/src/main/java/org/eol/globi/data/StudyImporterForBrose.java
@@ -0,0 +1,176 @@
+package org.eol.globi.data;
+
+import com.Ostermiller.util.LabeledCSVParser;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.eol.globi.domain.InteractType;
+import org.eol.globi.domain.Location;
+import org.eol.globi.domain.Specimen;
+import org.eol.globi.domain.Study;
+import org.eol.globi.domain.Term;
+import org.eol.globi.service.TermLookupService;
+import org.eol.globi.service.TermLookupServiceException;
+import org.eol.globi.service.UberonLookupService;
+import uk.me.jstott.jcoord.LatLng;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StudyImporterForBrose extends BaseStudyImporter {
+    private static final Log LOG = LogFactory.getLog(StudyImporterForBrose.class);
+
+    public static final String SOURCE = "Brose, U. et al., 2005. Body sizes of consumers and their resources. Ecology 86:2545. Available at: http://dx.doi.org/10.1890/07-1551.1 . Available at " + "http://www.esapubs.org/archive/ecol/E086/135/" + " .";
+    public static final String RESOURCE_PATH = "brose/bodysizes_2008.txt.gz";
+    public static final String REFERENCE_PATH = "brose/references.csv";
+    private TermLookupService termService = new UberonLookupService();
+
+    private static final Map<String, LatLng> LOC_MAP = new HashMap<String, LatLng>() {{
+        put("Country: United Kingdom; UTM: 51.24'N, 0.34'W; Silwood Park, Berkshire", new LatLng(51.24d, -0.34d));
+        put("Country: United Kingdom; UTM: 53.40'N, 0.59'W; Skipwith Common, North Yorkshire", new LatLng(53.40, -0.59));
+        put("Country: United Kingdom; the River Frome, Dorset;  UTM: 50.40'N, 2.11'W", new LatLng(50.40, -2.11));
+        put("Country: Australia; Mulgrave River; UTM: 17.08'S, 145.52'E", new LatLng(-17.08, 145.52));
+        put("Country: United States of America; UTM: 89.32'W, 46.13'N; Tuesday Lake, Michigan ", new LatLng(46.13, -89.32));
+        put("Country: United Kingdom; UTM: 51.05'N, 0.03'E; Broadstone Stream in Sussex", new LatLng(51.05, 0.03));
+        put("Country: Switzerland; Lake Neuch�tel", new LatLng(46.92518, 6.87572)); // see http://www.geonames.org/2659494/lac-de-neuchatel.html
+        put("Europe, Celtic Sea ecosystem", new LatLng(50, -9)); // see http://www.geonames.org/2960856/celtic-sea.html
+        put("Country: United Kingdom", new LatLng(54.75844, -2.69531)); // see http://www.geonames.org/2635167/united-kingdom-of-great-britain-and-northern-ireland.html
+        put("Antarctica: Eastern Weddell Sea", new LatLng(-75.0494, -47.2628)); // see http://www.geonames.org/4036624/weddell-sea.html
+        put("Africa, Benguela ecosystem", new LatLng(-12.854, 13.92825)); // see http://www.geonames.org/3351660/benguela.html
+        put("Country: United Kingdom; Sheffield;", new LatLng(53.41667, -1.5)); // see http://www.geonames.org/3333193/sheffield.html
+        put("Mid-Europe", new LatLng(48.69096, 9.14062)); // see http://www.geonames.org/6255148/europe.html
+        put("Country: United States of America; Atlantic coast", new LatLng(40.9777996, -79.5252906)); // point picked from google map: an educated guess
+        put("Country: Australia", new LatLng(-25, 135)); // see http://www.geonames.org/2077456/commonwealth-of-australia.html
+        put("Country: United States of America; John Muir Wilderness Area and Kings Canyon National Park, Sierra Nevada mountains, California", new LatLng(36.925347, -118.681823)); // see picked from map halfway point between john muir and kings canyon.
+        put("-999", null);
+        put("Europe, USA, Arctic, India", null); // too vague
+    }};
+
+    public StudyImporterForBrose(ParserFactory parserFactory, NodeFactory nodeFactory) {
+        super(parserFactory, nodeFactory);
+    }
+
+    @Override
+    public Study importStudy() throws StudyImporterException {
+        LabeledCSVParser dataParser;
+        try {
+            dataParser = parserFactory.createParser(RESOURCE_PATH, CharsetConstant.UTF8);
+        } catch (IOException e) {
+            throw new StudyImporterException("failed to read resource [" + RESOURCE_PATH + "]", e);
+        }
+        dataParser.changeDelimiter('\t');
+
+        Map<String, String> refMap = ReferenceUtil.buildRefMap(parserFactory, REFERENCE_PATH);
+
+        try {
+            while (dataParser.getLine() != null) {
+                if (importFilter.shouldImportRecord((long) dataParser.getLastLineNumber())) {
+                    importLine(dataParser, refMap);
+                }
+            }
+        } catch (IOException e) {
+            throw new StudyImporterException("problem importing study at line [" + dataParser.lastLineNumber() + "]", e);
+        }
+
+        return null;
+    }
+
+    private void importLine(LabeledCSVParser parser, Map<String, String> refMap) throws StudyImporterException {
+        Study localStudy = null;
+        try {
+            String shortReference = StringUtils.trim(parser.getValueByLabel("Link reference"));
+            if (!refMap.containsKey(shortReference)) {
+                throw new StudyImporterException("failed to find ref [" + shortReference + "] on line [" + parser.lastLineNumber() + "]");
+            }
+            String longReference = refMap.get(shortReference);
+            localStudy = nodeFactory.getOrCreateStudy("BROSE-" + shortReference, null, null, null, longReference, null, SOURCE);
+
+            String name = getName(parser, "Taxonomy consumer", "Common name(s) consumer");
+            if (StringUtils.isBlank(name)) {
+                getLogger().warn(localStudy, "found empty name on line [" + parser.lastLineNumber() + "]");
+            } else {
+                addInteractionForConsumer(parser, localStudy, name);
+            }
+        } catch (NodeFactoryException e) {
+            throw new StudyImporterException("problem creating nodes at line [" + parser.lastLineNumber() + "]", e);
+        } catch (NumberFormatException e) {
+            String message = "skipping record, found malformed field at line [" + parser.lastLineNumber() + "]: ";
+            if (localStudy != null) {
+                getLogger().warn(localStudy, message + e.getMessage());
+            }
+        }
+    }
+
+    private String getName(LabeledCSVParser parser, String primaryLabel, String secondaryLabel) {
+        String name = parser.getValueByLabel(primaryLabel);
+        if ("-999".equals(name)) {
+            name = parser.getValueByLabel(secondaryLabel);
+        }
+        return name;
+    }
+
+    private void addInteractionForConsumer(LabeledCSVParser parser, Study localStudy, String predatorName) throws NodeFactoryException, StudyImporterException {
+        Specimen consumer = nodeFactory.createSpecimen(predatorName);
+        addLifeStage(parser, consumer, "Lifestage consumer");
+
+
+        String locationString = parser.getValueByLabel("Geographic location");
+        LatLng latLng = LOC_MAP.get(StringUtils.trim(locationString));
+        if (latLng == null) {
+            getLogger().warn(localStudy, "failed to find location for [" + locationString + "]");
+        } else {
+            Location location = nodeFactory.getOrCreateLocation(latLng.getLat(), latLng.getLng(), null);
+            consumer.caughtIn(location);
+            String habitat = StringUtils.join(parser.getValueByLabel("General habitat"), " ", parser.getValueByLabel("Specific habitat"));
+            String habitatId = "BROSE:" + habitat.replaceAll("\\W", "_");
+            nodeFactory.getOrCreateEnvironments(location, habitatId, habitat);
+        }
+
+        String name = getName(parser, "Taxonomy resource", "Common name(s) resource");
+        if (StringUtils.isBlank(name) || StringUtils.length(name) < 2) {
+            String message = "found (near) empty prey name on line [" + parser.lastLineNumber() + "] + [" + name + "]";
+            LOG.warn(message);
+            getLogger().warn(localStudy, message);
+        } else {
+            Specimen resource = nodeFactory.createSpecimen(name);
+            addLifeStage(parser, resource, "Lifestage - resource");
+            String interactionType = parser.getValueByLabel("Type of feeding interaction");
+            Map<String, InteractType> typeMapping = new HashMap<String, InteractType>() {
+                {
+                    put("predacious", InteractType.PREYS_UPON);
+                    put("predator", InteractType.PREYS_UPON);
+                    put("herbivorous", InteractType.ATE);
+                    put("parasitoid", InteractType.PARASITE_OF);
+                    put("parasitic", InteractType.PARASITE_OF);
+                    put("bacterivorous", InteractType.ATE);
+                    put("omnivore", InteractType.ATE);
+                    put("detritivorous", InteractType.ATE);
+                    put("pathogen", InteractType.PATHOGEN_OF);
+                }
+            };
+            InteractType interactType = typeMapping.get(interactionType);
+            if (interactType == null) {
+                throw new StudyImporterException("found unsupported interaction type [" + interactionType + "]");
+            }
+            consumer.interactsWith(resource, interactType);
+        }
+
+        localStudy.collected(consumer);
+    }
+
+    private void addLifeStage(LabeledCSVParser parser, Specimen specimen, String label) throws StudyImporterException {
+        String lifeStageString = parser.getValueByLabel(label);
+        try {
+            List<Term> terms = termService.lookupTermByName(lifeStageString);
+            if (terms.size() == 0) {
+                throw new StudyImporterException("unsupported life stage [" + lifeStageString + "] on line [" + parser.getLastLineNumber() + "]");
+            }
+            specimen.setLifeStage(terms);
+        } catch (TermLookupServiceException e) {
+            throw new StudyImporterException(("failed to map life stage [" + lifeStageString + "]"));
+        }
+    }
+
+}
---
+++ eol-globi-data-tool/src/test/java/org/eol/globi/data/StudyImporterForBroseTest.java
@@ -0,0 +1,93 @@
+package org.eol.globi.data;
+
+import org.apache.commons.lang.StringUtils;
+import org.eol.globi.domain.Environment;
+import org.eol.globi.domain.Location;
+import org.eol.globi.domain.RelTypes;
+import org.eol.globi.domain.Specimen;
+import org.eol.globi.domain.Study;
+import org.eol.globi.domain.TaxonNode;
+import org.junit.Test;
+import org.neo4j.graphdb.Direction;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+import static junit.framework.Assert.assertTrue;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class StudyImporterForBroseTest extends GraphDBTestCase {
+
+    @Test
+    public void importHeadAndTail() throws IOException, NodeFactoryException, StudyImporterException {
+        final String headAndTail = "Link ID\tLink reference \tBody size reference\tGeographic location\tGeneral habitat\tSpecific habitat\tLink methodology\tBody size methodology\tTaxonomy consumer\tLifestage consumer\tCommon name(s) consumer\tMetabolic category consumer\tType of feeding interaction\tMinimum length (m) consumer\tMean length (m) consumer\tMaximum length (m) consumer\tMinimum mass (g) consumer\tMean mass (g) consumer\tMaximum mass (g) consumer\tTaxonomy resource\tLifestage - resource\tCommon name(s) resource\tMetabolic category resource\tMinimum length (m) resource\tMean length (m) resource\tMaximum length (m) resource\tMinimum mass (g) resource\tMean mass (g) resource\tMaximum mass (g) resource\tConsumer/resource body mass ratio\tNotes\n" +
+                "1\tYodzis (1998)\tYodzis (1998)\tAfrica, Benguela ecosystem\tmarine\tpelagic food web\tpublished account\t\"published account; expert; regression\"\t-999\tadults\tBacteria\theterotrophic bacteria\therbivorous\t-999\t-999\t-999\t-999\t0.00000001\t-999\t-999\tadults\tPhytoplankton\tphoto-autotroph\t-999\t-999\t-999\t-999\t0.0001\t-999\t0.0001\t-999\n" +
+                "2\tYodzis (1998)\tYodzis (1998)\tAfrica, Benguela ecosystem\tmarine\tpelagic food web\tpublished account\t\"published account; expert; regression\"\t-999\tadults\tBenthic carnivores\tinvertebrate\tpredacious\t-999\t-999\t-999\t-999\t10\t-999\t-999\tadults\tBenthic filter feeders\tinvertebrate\t-999\t-999\t-999\t-999\t10\t-999\t1\t-999\n" +
+                "3\tYodzis (1998)\tYodzis (1998)\tAfrica, Benguela ecosystem\tmarine\tpelagic food web\tpublished account\t\"published account; expert; regression\"\t-999\tadults\tMicrozooplankton\tinvertebrate\therbivorous\t-999\t-999\t-999\t-999\t0.0001\t-999\t-999\tadults\tPhytoplankton\tphoto-autotroph\t-999\t-999\t-999\t-999\t0.0001\t-999\t1\t-999\n" +
+                "4\tYodzis (1998)\tYodzis (1998)\tAfrica, Benguela ecosystem\tmarine\tpelagic food web\tpublished account\t\"published account; expert; regression\"\t-999\tadults\tMicrozooplankton\tinvertebrate\tbacterivorous\t-999\t-999\t-999\t-999\t0.0001\t-999\t-999\tadults\tBacteria\theterotrophic bacteria\t-999\t-999\t-999\t-999\t0.00000001\t-999\t10000\t-999\n" +
+                "5\tYodzis (1998)\tYodzis (1998)\tAfrica, Benguela ecosystem\tmarine\tpelagic food web\tpublished account\t\"published account; expert; regression\"\t-999\tadults\tMicrozooplankton\tinvertebrate\tpredacious\t-999\t-999\t-999\t-999\t0.0001\t-999\t-999\tadults\tMicrozooplankton\tinvertebrate\t-999\t-999\t-999\t-999\t0.0001\t-999\t1\t-999\n" +
+                "6\tYodzis (1998)\tYodzis (1998)\tAfrica, Benguela ecosystem\tmarine\tpelagic food web\tpublished account\t\"published account; expert; regression\"\t-999\tadults\tMesozooplankton\tinvertebrate\therbivorous\t-999\t-999\t-999\t-999\t0.01\t-999\t-999\tadults\tPhytoplankton\tphoto-autotroph\t-999\t-999\t-999\t-999\t0.0001\t-999\t100\t-999\n" +
+                "7\tYodzis (1998)\tYodzis (1998)\tAfrica, Benguela ecosystem\tmarine\tpelagic food web\tpublished account\t\"published account; expert; regression\"\t-999\tadults\tMesozooplankton\tinvertebrate\tpredacious\t-999\t-999\t-999\t-999\t0.01\t-999\t-999\tadults\tMicrozooplankton\tinvertebrate\t-999\t-999\t-999\t-999\t0.0001\t-999\t100\t-999\n" +
+                "8\tYodzis (1998)\tYodzis (1998)\tAfrica, Benguela ecosystem\tmarine\tpelagic food web\tpublished account\t\"published account; expert; regression\"\t-999\tadults\tMacrozooplankton\tinvertebrate\therbivorous\t-999\t-999\t-999\t-999\t1\t-999\t-999\tadults\tPhytoplankton\tphoto-autotroph\t-999\t-999\t-999\t-999\t0.0001\t-999\t10000\t-999\n" +
+                "9\tYodzis (1998)\tYodzis (1998)\tAfrica, Benguela ecosystem\tmarine\tpelagic food web\tpublished account\t\"published account; expert; regression\"\t-999\tadults\tMacrozooplankton\tinvertebrate\tpredacious\t-999\t-999\t-999\t-999\t1\t-999\t-999\tadults\tMesozooplankton\tinvertebrate\t-999\t-999\t-999\t-999\t0.01\t-999\t100\t-999\n" +
+                "16857\tCohen et al. (2005)\tCohen et al. (2005)\t\"Country: United Kingdom; UTM: 51.24'N, 0.34'W; Silwood Park, Berkshire\"\tterrestrial\tabandoned field\trearing\t\"measurement; regression\"\tPraon dorsale\tadult\tparasitoid wasp\tinvertebrate\tparasitoid\t0.002034483\t0.002197044\t0.002344828\t-999\t-999\t-999\tAcyrthosiphon pisum\tadult\taphid\tinvertebrate\t0.002448276\t0.00264532\t0.002793103\t-999\t-999\t-999\t0.614787036\t\"newly emerged adult parsitoids were measured; resource length was measured without cauda; for data on individual body sizes and body sizes of hyperparasitoids and mummy parasitoids see Cohen et al. (2005); 7 pairs of individuals were measured\"\n" +
+                "16858\tCohen et al. (2005)\tCohen et al. (2005)\t\"Country: United Kingdom; UTM: 51.24'N, 0.34'W; Silwood Park, Berkshire\"\tterrestrial\tabandoned field\trearing\t\"measurement; regression\"\tAphidius urticae\tadult\tparasitoid wasp\tinvertebrate\tparasitoid\t0.002\t0.002665025\t0.003517241\t-999\t-999\t-999\tMacrosiphum funestum\tadult\taphid\tinvertebrate\t0.002482759\t0.003214286\t0.003965517\t-999\t-999\t-999\t0.61203447\t\"newly emerged adult parsitoids were measured; resource length was measured without cauda; for data on individual body sizes and body sizes of hyperparasitoids and mummy parasitoids see Cohen et al. (2005); 14 pairs of individuals were measured\"\n" +
+                "16859\tCohen et al. (2005)\tCohen et al. (2005)\t\"Country: United Kingdom; UTM: 51.24'N, 0.34'W; Silwood Park, Berkshire\"\tterrestrial\tabandoned field\trearing\t\"measurement; regression\"\tPraon dorsale\tadult\tparasitoid wasp\tinvertebrate\tparasitoid\t0.001965517\t0.001965517\t0.001965517\t-999\t-999\t-999\tAmphorophora rubi\tadult\taphid\tinvertebrate\t0.002482759\t0.002482759\t0.002482759\t-999\t-999\t-999\t0.542226802\t\"newly emerged adult parsitoids were measured; resource length was measured without cauda; for data on individual body sizes and body sizes of hyperparasitoids and mummy parasitoids see Cohen et al. (2005); 1 pair of individuals were measured\"\n" +
+                "16860\tCohen et al. (2005)\tCohen et al. (2005)\t\"Country: United Kingdom; UTM: 51.24'N, 0.34'W; Silwood Park, Berkshire\"\tterrestrial\tabandoned field\trearing\t\"measurement; regression\"\tPraon dorsale\tadult\tparasitoid wasp\tinvertebrate\tparasitoid\t0.001896552\t0.001896552\t0.001896552\t-999\t-999\t-999\tMegoura viciaei\tadult\taphid\tinvertebrate\t0.002862069\t0.002862069\t0.002862069\t-999\t-999\t-999\t0.340224547\t\"newly emerged adult parsitoids were measured; resource length was measured without cauda; for data on individual body sizes and body sizes of hyperparasitoids and mummy parasitoids see Cohen et al. (2005); 1 pair of individuals were measured\"\n" +
+                "16861\tCohen et al. (2005)\tCohen et al. (2005)\t\"Country: United Kingdom; UTM: 51.24'N, 0.34'W; Silwood Park, Berkshire\"\tterrestrial\tabandoned field\trearing\t\"measurement; regression\"\tAphelinus abdominalis\tadult\tparasitoid wasp\tinvertebrate\tparasitoid\t0.000793103\t0.000926724\t0.001206897\t-999\t-999\t-999\tSitobion fragariae / Sitobion avenae\tnymphs\taphid\tinvertebrate\t0.001310345\t0.001599138\t0.001896552\t-999\t-999\t-999\t0.239457874\t\"newly emerged adult parsitoids were measured; resource length was measured without cauda; for data on individual body sizes and body sizes of hyperparasitoids and mummy parasitoids see Cohen et al. (2005); 16 pairs of individuals were measured\"\n" +
+                "16862\tCohen et al. (2005)\tCohen et al. (2005)\t\"Country: United Kingdom; UTM: 51.24'N, 0.34'W; Silwood Park, Berkshire\"\tterrestrial\tabandoned field\trearing\t\"measurement; regression\"\tAphelinus abdominalis\tadult\tparasitoid wasp\tinvertebrate\tparasitoid\t0.000965517\t0.000965517\t0.000965517\t-999\t-999\t-999\tAmphorophora rubi\tadult\taphid\tinvertebrate\t0.001689655\t0.001689655\t0.001689655\t-999\t-999\t-999\t0.230802396\t\"newly emerged adult parsitoids were measured; resource length was measured without cauda; for data on individual body sizes and body sizes of hyperparasitoids and mummy parasitoids see Cohen et al. (2005); 1 pair of individuals were measured\"\n" +
+                "16863\tCohen et al. (2005)\tCohen et al. (2005)\t\"Country: United Kingdom; UTM: 51.24'N, 0.34'W; Silwood Park, Berkshire\"\tterrestrial\tabandoned field\trearing\t\"measurement; regression\"\tAphelinus abdominalis\tadult\tparasitoid wasp\tinvertebrate\tparasitoid\t0.000896552\t0.001132626\t0.001310345\t-999\t-999\t-999\tSitobion fragariae / Sitobion avenae\tadult\taphid\tinvertebrate\t0.001448276\t0.00201061\t0.002517241\t-999\t-999\t-999\t0.222324699\t\"newly emerged adult parsitoids were measured; resource length was measured without cauda; for data on individual body sizes and body sizes of hyperparasitoids and mummy parasitoids see Cohen et al. (2005); 13 pairs of individuals were measured\"\n" +
+                "16864\tCohen et al. (2005)\tCohen et al. (2005)\t\"Country: United Kingdom; UTM: 51.24'N, 0.34'W; Silwood Park, Berkshire\"\tterrestrial\tabandoned field\trearing\t\"measurement; regression\"\tAphelinus abdominalis\tadult\tparasitoid wasp\tinvertebrate\tparasitoid\t0.001068966\t0.001206897\t0.001344828\t-999\t-999\t-999\tAcyrthosiphon pisum\tnymphs\taphid\tinvertebrate\t0.002034483\t0.002206897\t0.00237931\t-999\t-999\t-999\t0.205715371\t\"newly emerged adult parsitoids were measured; resource length was measured without cauda; for data on individual body sizes and body sizes of hyperparasitoids and mummy parasitoids see Cohen et al. (2005); 2 pairs of individuals were measured\"\n" +
+                "16865\tCohen et al. (2005)\tCohen et al. (2005)\t\"Country: United Kingdom; UTM: 51.24'N, 0.34'W; Silwood Park, Berkshire\"\tterrestrial\tabandoned field\trearing\t\"measurement; regression\"\tAphelinus abdominalis\tadult\tparasitoid wasp\tinvertebrate\tparasitoid\t0.001103448\t0.00112069\t0.001137931\t-999\t-999\t-999\tMetopolophium albidum\tadult\taphid\tinvertebrate\t0.002034483\t0.002172414\t0.002310345\t-999\t-999\t-999\t0.176547741\t\"newly emerged adult parsitoids were measured; resource length was measured without cauda; for data on individual body sizes and body sizes of hyperparasitoids and mummy parasitoids see Cohen et al. (2005); 2 pairs of individuals were measured\"\n" +
+                "16866\tCohen et al. (2005)\tCohen et al. (2005)\t\"Country: United Kingdom; UTM: 51.24'N, 0.34'W; Silwood Park, Berkshire\"\tterrestrial\tabandoned field\trearing\t\"measurement; regression\"\tAphelinus varipes\tadult\tparasitoid wasp\tinvertebrate\tparasitoid\t0.000896552\t0.000896552\t0.000896552\t-999\t-999\t-999\tSitobion fragariae / Sitobion avenae\tnymphs\taphid\tinvertebrate\t0.001965517\t0.001965517\t0.001965517\t-999\t-999\t-999\t0.127890431\t\"newly emerged adult parsitoids were measured; resource length was measured without cauda; for data on individual body sizes and body sizes of hyperparasitoids and mummy parasitoids see Cohen et al. (2005); 1 pair of individuals were measured\"\n";
+
+
+        TestParserFactory parserFactory = new TestParserFactory(new HashMap<String, String>() {{
+            put(StudyImporterForBrose.RESOURCE_PATH, headAndTail);
+            put(StudyImporterForBrose.REFERENCE_PATH, "short,full\nCohen et al. (2005),something long\nYodzis (1998),something longer");
+        }
+        });
+        StudyImporter importer = new StudyImporterForBrose(parserFactory, nodeFactory);
+        importer.importStudy();
+
+        TaxonNode taxon = nodeFactory.findTaxon("Praon dorsale");
+        Iterable<Relationship> relationships = taxon.getUnderlyingNode().getRelationships(Direction.INCOMING, RelTypes.CLASSIFIED_AS);
+        for (Relationship relationship : relationships) {
+            Node predatorSpecimenNode = relationship.getStartNode();
+            assertThat((String) predatorSpecimenNode.getProperty(Specimen.LIFE_STAGE_LABEL), is("post-juvenile adult stage"));
+            assertThat((String) predatorSpecimenNode.getProperty(Specimen.LIFE_STAGE_ID), is("UBERON:0000113"));
+
+        }
+        assertThat(taxon, is(notNullValue()));
+        assertThat(nodeFactory.findTaxon("Aphelinus abdominalis"), is(notNullValue()));
+
+        Location location = nodeFactory.findLocation(51.24, -0.34, null);
+        assertThat("missing location", location, is(notNullValue()));
+
+        List<Environment> environments = location.getEnvironments();
+        assertThat(environments.size(), is(1));
+        assertThat(environments.get(0).getExternalId(), is("TEST:terrestrial abandoned field"));
+        assertThat(environments.get(0).getName(), is("terrestrial abandoned field"));
+
+    }
+
+    @Test
+    public void importAll() throws StudyImporterException {
+        StudyImporterForBrose studyImporterForBrose = new StudyImporterForBrose(new ParserFactoryImpl(), nodeFactory);
+        studyImporterForBrose.importStudy();
+
+        List<Study> studies = NodeFactory.findAllStudies(getGraphDb());
+        assertTrue(studies.size() > 0);
+        for (Study study : studies) {
+            assertThat(study.getTitle(), is(notNullValue()));
+            assertThat(study.getSource(), is(notNullValue()));
+            assertThat(StringUtils.isBlank(study.getDescription()), is(false));
+        }
+    }
+
+}
