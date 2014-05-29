ebcc18329bd02de9a730f8e727214b48c2bd60c5
Jorrit Poelen <jhpoelen@gmail.com>
2014-05-29 14:01:19 -0700

    refactor fishbase parser

--- eol-globi-data-tool/src/main/java/org/eol/globi/data/StudyImporterForFishbase.java
+++ eol-globi-data-tool/src/main/java/org/eol/globi/data/StudyImporterForFishbase.java
@@ -4,19 +4,13 @@ import com.Ostermiller.util.LabeledCSVParser;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.eol.globi.domain.Location;
 import org.eol.globi.domain.Specimen;
 import org.eol.globi.domain.Study;
-import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.ISODateTimeFormat;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Relationship;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class StudyImporterForFishbase extends BaseStudyImporter {
@@ -39,57 +33,9 @@ public class StudyImporterForFishbase extends BaseStudyImporter {
             while (parser.getLine() != null) {
                 int lastLineNumber = parser.getLastLineNumber();
                 if (importFilter.shouldImportRecord((long) lastLineNumber)) {
-                    String foodName = StringUtils.join(new String[] {parser.getValueByLabel("food item genus"),
-                            parser.getValueByLabel("food item species")}, " ");
-                    if (StringUtils.isBlank(foodName) || StringUtils.contains(foodName, "NULL")) {
-                        foodName = parser.getValueByLabel("food III");
-                    }
-
-
-                    String consumerName = StringUtils.join(new String[] {parser.getValueByLabel("consumer genus"),
-                            parser.getValueByLabel("consumer species")}, " ");
-
-
-                    String author = parser.getValueByLabel("author");
-                    String year = parser.getValueByLabel("year");
-                    String title = parser.getValueByLabel("title");
-                    Study study = nodeFactory.getOrCreateStudy("Fishbase-" + author + year,
-                            author,
-                            "",
-                            "",
-                            title
-                            , year
-                            , "Database export shared by http://fishbase.org in December 2013. For use by Brian Hayden and Jorrit Poelen only.", null);
-
-                    Specimen predator;
-                    try {
-                        predator = nodeFactory.createSpecimen(consumerName);
-                        predator.ate(nodeFactory.createSpecimen(foodName));
-                    } catch (NodeFactoryException e) {
-                        throw new StudyImporterException("failed to create specimen [" + consumerName + "] on line [" + parser.lastLineNumber() + 1 + "]", e);
-                    }
-                    study.collected(predator);
-
-
-                    parser.getValueByLabel("locality");
-                    parser.getValueByLabel("countryCode");
-                    String latitude = StringUtils.replace(parser.getValueByLabel("latitude"), "NULL", "");
-                    String longitude = StringUtils.replace(parser.getValueByLabel("longitude"), "NULL", "");
-
-                    if (StringUtils.isNotBlank(latitude) && StringUtils.isNotBlank(longitude))  {
-                        try {
-                            predator.caughtIn(nodeFactory.getOrCreateLocation(Double.parseDouble(latitude),
-                                    Double.parseDouble(longitude), null));
-                        } catch (NodeFactoryException e) {
-                            throw new StudyImporterException("failed to create location using [" + latitude + "] and [" + longitude + "] on line [" + parser.lastLineNumber() + 1 + "]", e);
-
-                        } catch (NumberFormatException e) {
-                            throw new StudyImporterException("failed to create location using [" + latitude + "] and [" + longitude + "] on line [" + parser.lastLineNumber() + 1 + "]", e);
-                        }
-                    }
-
-
-
+                    Study study = parseStudy(parser);
+                    Specimen consumer = parseInteraction(parser, study);
+                    associateLocation(parser, consumer);
                 }
             }
         } catch (IOException e) {
@@ -97,5 +43,60 @@ public class StudyImporterForFishbase extends BaseStudyImporter {
         }
 
         return null;
+    }
+
+    private void associateLocation(LabeledCSVParser parser, Specimen consumer) throws StudyImporterException {
+        parser.getValueByLabel("locality");
+        parser.getValueByLabel("countryCode");
+        String latitude = StringUtils.replace(parser.getValueByLabel("latitude"), "NULL", "");
+        String longitude = StringUtils.replace(parser.getValueByLabel("longitude"), "NULL", "");
+
+        if (StringUtils.isNotBlank(latitude) && StringUtils.isNotBlank(longitude)) {
+            try {
+                consumer.caughtIn(nodeFactory.getOrCreateLocation(Double.parseDouble(latitude),
+                        Double.parseDouble(longitude), null));
+            } catch (NodeFactoryException e) {
+                throw new StudyImporterException("failed to create location using [" + latitude + "] and [" + longitude + "] on line [" + parser.lastLineNumber() + 1 + "]", e);
+
+            } catch (NumberFormatException e) {
+                throw new StudyImporterException("failed to create location using [" + latitude + "] and [" + longitude + "] on line [" + parser.lastLineNumber() + 1 + "]", e);
+            }
+        }
+    }
+
+    private Specimen parseInteraction(LabeledCSVParser parser, Study study) throws StudyImporterException {
+        Specimen consumer;
+        try {
+            String consumerName = StringUtils.join(new String[]{parser.getValueByLabel("consumer genus"),
+                    parser.getValueByLabel("consumer species")}, " ");
+            consumer = nodeFactory.createSpecimen(consumerName);
+            consumer.ate(nodeFactory.createSpecimen(parseFoodName(parser)));
+            study.collected(consumer);
+        } catch (NodeFactoryException e) {
+            throw new StudyImporterException("failed to create specimens on line [" + parser.lastLineNumber() + 1 + "]", e);
+        }
+        return consumer;
+    }
+
+    private Study parseStudy(LabeledCSVParser parser) {
+        String author = parser.getValueByLabel("author");
+        String year = parser.getValueByLabel("year");
+        String title = parser.getValueByLabel("title");
+        return nodeFactory.getOrCreateStudy("Fishbase-" + author + year,
+                author,
+                "",
+                "",
+                title
+                , year
+                , "Database export shared by http://fishbase.org in December 2013. For use by Brian Hayden and Jorrit Poelen only.", null);
+    }
+
+    private String parseFoodName(LabeledCSVParser parser) {
+        String foodName = StringUtils.join(new String[]{parser.getValueByLabel("food item genus"),
+                parser.getValueByLabel("food item species")}, " ");
+        if (StringUtils.isBlank(foodName) || StringUtils.contains(foodName, "NULL")) {
+            foodName = parser.getValueByLabel("food III");
+        }
+        return foodName;
     }
 }
