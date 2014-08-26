1438b275d8dcd0163fb41885cbff4e43f9bb6cf6
Jorrit Poelen <jhpoelen@gmail.com>
2014-08-26 12:52:51 -0700

    fix test error

--- eol-globi-data-tool/src/test/java/org/eol/globi/data/StudyImporterForGoMexSITest.java
+++ eol-globi-data-tool/src/test/java/org/eol/globi/data/StudyImporterForGoMexSITest.java
@@ -7,6 +7,7 @@ import org.eol.globi.domain.RelTypes;
 import org.eol.globi.domain.Specimen;
 import org.eol.globi.domain.Study;
 import org.eol.globi.domain.TaxonNode;
+import org.eol.globi.util.ExternalIdUtil;
 import org.junit.Test;
 import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.Node;
@@ -37,7 +38,7 @@ public class StudyImporterForGoMexSITest extends GraphDBTestCase {
 
         assertNotNull(study);
         assertThat(study.getTitle(), is("Divita et al 1983"));
-        assertThat(study.getExternalId(), is("GAME:2689"));
+        assertThat(study.getExternalId(), is(ExternalIdUtil.infoURLForExternalId("GAME:2689")));
         assertThat(study.getContributor(), is("Regina Divita, Mischelle Creel, Peter Sheridan"));
         assertThat(study.getPublicationYear(), is("1983"));
         assertThat(study.getInstitution(), is(""));
