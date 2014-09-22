c4b672b48696dd451b13ae29c797f1d3cb3c93c7
Jorrit Poelen <jhpoelen@gmail.com>
2014-09-22 10:23:54 -0700

    added Stona et al. fish parasite dataset. see issue #96

---
+++ eol-globi-data-tool/src/main/java/org/eol/globi/data/StudyImporterForStrona.java
@@ -0,0 +1,63 @@
+package org.eol.globi.data;
+
+import com.Ostermiller.util.LabeledCSVParser;
+import org.apache.commons.lang3.StringUtils;
+import org.eol.globi.domain.InteractType;
+import org.eol.globi.domain.Specimen;
+import org.eol.globi.domain.Study;
+
+import java.io.IOException;
+
+public class StudyImporterForStrona extends BaseStudyImporter {
+    public static final String SOURCE = "Giovanni Strona, Maria Lourdes D. Palomares, Nicolas Bailly, Paolo Galli, and Kevin D. Lafferty. 2013. Host range, host ecology, and distribution of more than 11800 fish parasite species. Ecology 94:544. http://dx.doi.org/10.1890/12-1419.1";
+    public static final String RESOURCE_PATH = "http://www.esapubs.org/archive/ecol/E094/045/FPEDB.csv";
+
+    public StudyImporterForStrona(ParserFactory parserFactory, NodeFactory nodeFactory) {
+        super(parserFactory, nodeFactory);
+    }
+
+    @Override
+    public Study importStudy() throws StudyImporterException {
+        LabeledCSVParser dataParser;
+        try {
+            dataParser = parserFactory.createParser(RESOURCE_PATH, CharsetConstant.UTF8);
+        } catch (IOException e) {
+            throw new StudyImporterException("failed to read resource [" + RESOURCE_PATH + "]", e);
+        }
+        try {
+            Study study = nodeFactory.getOrCreateStudy("strona2013"
+                    , SOURCE + " . " + ReferenceUtil.createLastAccessedString(RESOURCE_PATH)
+                    , "http://dx.doi.org/10.1890/12-1419.1");
+            while (dataParser.getLine() != null) {
+                if (importFilter.shouldImportRecord((long) dataParser.getLastLineNumber())) {
+                    try {
+                        String parasiteName = StringUtils.trim(dataParser.getValueByLabel("P_SP"));
+                        String hostName = StringUtils.trim(dataParser.getValueByLabel("H_SP"));
+                        if (areNamesAvailable(parasiteName, hostName)) {
+                            Specimen parasite = nodeFactory.createSpecimen(parasiteName);
+                            Specimen host = nodeFactory.createSpecimen(hostName);
+                            parasite.interactsWith(host, InteractType.PARASITE_OF);
+                            study.collected(parasite);
+                        }
+                    } catch (NodeFactoryException e) {
+                        throw new StudyImporterException("failed to import line [" + (dataParser.lastLineNumber() + 1) + "]", e);
+                    } catch (NumberFormatException e) {
+                        throw new StudyImporterException("failed to import line [" + (dataParser.lastLineNumber() + 1) + "]", e);
+                    }
+                }
+
+            }
+        } catch (IOException e) {
+            throw new StudyImporterException("problem importing [" + RESOURCE_PATH + "]", e);
+
+
+        }
+        return null;
+    }
+
+    protected boolean areNamesAvailable(String parasiteName, String hostName) {
+        return StringUtils.isNotBlank(parasiteName) && StringUtils.isNotBlank(hostName)
+                && !StringUtils.equals("na", parasiteName) && !StringUtils.equals("na", hostName);
+    }
+
+}
---
+++ eol-globi-data-tool/src/test/java/org/eol/globi/data/StudyImporterForStronaTest.java
@@ -0,0 +1,38 @@
+package org.eol.globi.data;
+
+import org.eol.globi.domain.Study;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.containsString;
+
+public class StudyImporterForStronaTest extends GraphDBTestCase {
+
+
+    @Test
+    public void importFirst200() throws NodeFactoryException, StudyImporterException {
+        StudyImporter importer = new StudyImporterForStrona(new ParserFactoryImpl(), nodeFactory);
+        importer.setFilter(new ImportFilter() {
+            @Override
+            public boolean shouldImportRecord(Long recordNumber) {
+                return recordNumber < 200;
+            }
+        });
+        importer.importStudy();
+
+        List<Study> allStudies = NodeFactory.findAllStudies(getGraphDb());
+        assertThat(allStudies.size(), is(1));
+
+
+        assertThat(nodeFactory.findTaxonByName("Aidablennius sphynx"), is(notNullValue()));
+        assertThat(nodeFactory.findTaxonByName("Acanthocephaloides incrassatus"), is(notNullValue()));
+
+        assertThat(allStudies.get(0).getSource(), containsString(" Accessed at"));
+    }
+
+
+}
