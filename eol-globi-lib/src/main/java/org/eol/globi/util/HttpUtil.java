1e5ae5917d6dcfe4c32e138276555f8d05cb6273
Jorrit Poelen <jhpoelen@gmail.com>
2015-02-27 10:00:13 -0800

    downgrade httpclient due to certificate issue

--- eol-globi-lib/src/main/java/org/eol/globi/util/HttpUtil.java
+++ eol-globi-lib/src/main/java/org/eol/globi/util/HttpUtil.java
@@ -2,16 +2,8 @@ package org.eol.globi.util;
 
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
-import org.apache.http.config.ConnectionConfig;
-import org.apache.http.impl.client.AutoRetryHttpClient;
-import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
 import org.apache.http.impl.client.DefaultServiceUnavailableRetryStrategy;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.client.cache.CacheConfig;
-import org.apache.http.impl.client.cache.CachingHttpClientBuilder;
-import org.apache.http.impl.client.cache.ExponentialBackOffSchedulingStrategy;
-import org.apache.http.params.HttpConnectionParams;
 
 public class HttpUtil {
 
@@ -28,8 +20,7 @@ public class HttpUtil {
                 .build();
         HttpClientBuilder httpClientBuilder = HttpClientBuilder.create()
                 .setServiceUnavailableRetryStrategy(new DefaultServiceUnavailableRetryStrategy(10, 5 * 1000))
-                .setDefaultRequestConfig(config)
-                .setRetryHandler(new DefaultHttpRequestRetryHandler(5, true));
+                .setDefaultRequestConfig(config);
         return httpClientBuilder.build();
     }
 }
