cf95cebc2ed39e5d4dfc2d30baa8cba71c48413b
Jorrit Poelen <jhpoelen@gmail.com>
2015-03-06 07:56:28 -0800

    stub geonames service in import test

--- eol-globi-data-tool/src/test/java/org/eol/globi/data/StudyImporterForCruaudTest.java
+++ eol-globi-data-tool/src/test/java/org/eol/globi/data/StudyImporterForCruaudTest.java
@@ -1,9 +1,12 @@
 package org.eol.globi.data;
 
 import org.eol.globi.domain.Study;
+import org.eol.globi.geo.LatLng;
+import org.eol.globi.service.GeoNamesService;
 import org.eol.globi.util.NodeUtil;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
@@ -13,17 +16,28 @@ import static org.junit.Assert.assertThat;
 public class StudyImporterForCruaudTest extends GraphDBTestCase {
 
     @Test
-        public void importAll() throws NodeFactoryException, StudyImporterException {
-            StudyImporter importer = new StudyImporterForCruaud(new ParserFactoryImpl(), nodeFactory);
-            importer.importStudy();
+    public void importAll() throws NodeFactoryException, StudyImporterException {
+        StudyImporterForCruaud importer = new StudyImporterForCruaud(new ParserFactoryImpl(), nodeFactory);
+        importer.setGeoNamesService(new GeoNamesService() {
+            @Override
+            public boolean hasTermForLocale(String locality) {
+                return true;
+            }
 
-            List<Study> allStudies = NodeUtil.findAllStudies(getGraphDb());
-            assertThat(allStudies.size(), is(1));
+            @Override
+            public LatLng findLatLng(String locality) throws IOException {
+                return new LatLng(0,0);
+            }
+        });
+        importer.importStudy();
+
+        List<Study> allStudies = NodeUtil.findAllStudies(getGraphDb());
+        assertThat(allStudies.size(), is(1));
 
 
-            assertThat(nodeFactory.findTaxonByName("Agaon sp."), is(notNullValue()));
-            assertThat(nodeFactory.findTaxonByName("Ficus chapaensis"), is(notNullValue()));
+        assertThat(nodeFactory.findTaxonByName("Agaon sp."), is(notNullValue()));
+        assertThat(nodeFactory.findTaxonByName("Ficus chapaensis"), is(notNullValue()));
 
-            assertThat(allStudies.get(0).getSource(), is(notNullValue()));
-        }
+        assertThat(allStudies.get(0).getSource(), is(notNullValue()));
+    }
 }
