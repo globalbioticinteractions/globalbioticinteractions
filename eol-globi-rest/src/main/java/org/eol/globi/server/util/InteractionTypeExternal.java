392ae6f8afe209e5331e77c796474ae87d58cc5e
Jorrit Poelen <jhpoelen@gmail.com>
2015-07-31 09:43:23 -0700

    make flower visitors available in interactions. see issue #146

--- eol-globi-rest/src/main/java/org/eol/globi/server/util/InteractionTypeExternal.java
+++ eol-globi-rest/src/main/java/org/eol/globi/server/util/InteractionTypeExternal.java
@@ -19,8 +19,8 @@ public enum InteractionTypeExternal {
     VECTOR_OF(CypherQueryBuilder.INTERACTION_VECTOR_OF, "vector", "pathogen", InteractType.VECTOR_OF),
     HAS_VECTOR(CypherQueryBuilder.INTERACTION_HAS_VECTOR, "pathogen", "vector", InteractType.HAS_VECTOR),
     SYMBIONT_OF(CypherQueryBuilder.INTERACTION_SYMBIONT_OF, "source", "target", InteractType.SYMBIONT_OF),
-    FLOWERS_VISITED_BY(CypherQueryBuilder.INTERACTION_FLOWERS_VISITED_BY, "plant", "visitor", InteractType.SYMBIONT_OF),
-    VISITS_FLOWERS_OF(CypherQueryBuilder.INTERACTION_VISITS_FLOWERS_OF, "visitor", "plant", InteractType.SYMBIONT_OF),
+    FLOWERS_VISITED_BY(CypherQueryBuilder.INTERACTION_FLOWERS_VISITED_BY, "plant", "visitor", InteractType.FLOWERS_VISITED_BY),
+    VISITS_FLOWERS_OF(CypherQueryBuilder.INTERACTION_VISITS_FLOWERS_OF, "visitor", "plant", InteractType.VISITS_FLOWERS_OF),
     INTERACTS_WITH(CypherQueryBuilder.INTERACTION_INTERACTS_WITH, "source", "target", InteractType.INTERACTS_WITH);
 
     InteractionTypeExternal(String label, String source, String target, InteractType interactType) {
