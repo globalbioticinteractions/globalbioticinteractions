60994a2703364d98686ab15e55209bc5eaa5da23
Jorrit Poelen <jhpoelen@gmail.com>
2015-08-31 16:30:09 -0700

    column mapping for Wood et al. 2015. Related to issue #159

---
+++ eol-globi-data-tool/src/main/java/org/eol/globi/data/StudyImporterWood.java
@@ -0,0 +1,65 @@
+package org.eol.globi.data;
+
+import com.Ostermiller.util.CSVParser;
+import com.Ostermiller.util.LabeledCSVParser;
+import org.apache.commons.lang3.StringUtils;
+import org.eol.globi.domain.Study;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class StudyImporterWood extends BaseStudyImporter {
+
+    public StudyImporterWood(ParserFactory parserFactory, NodeFactory nodeFactory) {
+        super(parserFactory, nodeFactory);
+    }
+
+    @Override
+    public Study importStudy() throws StudyImporterException {
+        return null;
+    }
+
+    public static List<Map<String, String>> importLinks(InputStream inputStream) throws IOException {
+        LabeledCSVParser parser = new LabeledCSVParser(new CSVParser(inputStream));
+
+        ArrayList<Map<String, String>> mappedLines = new ArrayList<Map<String, String>>();
+        while (parser.getLine() != null) {
+            Map<String, String> e = importLink(parser);
+            if (e != null) {
+                mappedLines.add(e);
+            }
+        }
+        return mappedLines;
+    }
+
+
+    private static Map<String, String> importLink(LabeledCSVParser parser) {
+        Map<String, String> link = new TreeMap<String, String>();
+        addTSN(parser, link, "PredTSN", "source_taxon_external_id");
+        link.put("source_taxon_name", parser.getValueByLabel("PredName"));
+        addTSN(parser, link, "PreyTSN", "target_taxon_external_id");
+        link.put("target_taxon_name", parser.getValueByLabel("PreyName"));
+        link.put("study_source_citation", "Wood SA, Russell R, Hanson D, Williams RJ, Dunne JA (2015) Data from: Effects of spatial scale of sampling on food web structure. Dryad Digital Repository. http://dx.doi.org/10.5061/dryad.g1qr6");
+        link.put("study_citation", "Wood SA, Russell R, Hanson D, Williams RJ, Dunne JA (2015) Effects of spatial scale of sampling on food web structure. Ecology and Evolution, online in advance of print. http://dx.doi.org/10.1002/ece3.1640");
+        link.put("study_doi", "doi:10.1002/ece3.1640");
+        link.put("study_url", "http://dx.doi.org/10.1002/ece3.1640");
+        link.put("locality_name", "Sanak Island, Alaska, USA");
+        link.put("locality_id", "GEONAMES:5873327");
+        link.put("latitude", "54.42972");
+        link.put("longitude", "-162.70889");
+        link.put("interaction_type_name", "preysOn");
+        link.put("interaction_type_id", "RO:0002439");
+        return link;
+    }
+
+    private static void addTSN(LabeledCSVParser parser, Map<String, String> link, String tsn, String tsnLabel) {
+        String tsnValue = parser.getValueByLabel(tsn);
+        if (!StringUtils.startsWith(tsnValue, "san")) {
+            link.put(tsnLabel, "ITIS:" + tsnValue);
+        }
+    }
+}
---
+++ eol-globi-data-tool/src/test/java/org/eol/globi/data/StudyImporterWoodTest.java
@@ -0,0 +1,54 @@
+package org.eol.globi.data;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class StudyImporterWoodTest extends GraphDBTestCase {
+
+    @Test
+    public void importLines() throws IOException {
+        List<Map<String, String>> maps = StudyImporterWood.importLinks(IOUtils.toInputStream(firstFewLines()));
+        assertThat(maps.size(), is(5));
+        Map<String, String> firstLink = maps.get(0);
+        assertThat(firstLink.get("source_taxon_external_id"), is("ITIS:93294"));
+        assertThat(firstLink.get("source_taxon_name"), is("Amphipoda"));
+        assertThat(firstLink.get("target_taxon_external_id"), is("ITIS:10824"));
+        assertThat(firstLink.get("target_taxon_name"), is("Pilayella littoralis"));
+        assertStaticInfo(firstLink);
+
+        Map<String, String> secondLink = maps.get(1);
+        assertThat(secondLink.get("source_taxon_external_id"), is(nullValue()));
+        assertThat(secondLink.get("source_taxon_name"), is("Phytoplankton complex"));
+        assertStaticInfo(secondLink);
+    }
+
+    protected void assertStaticInfo(Map<String, String> firstLink) {
+        assertThat(firstLink.get("study_source_citation"), is("Wood SA, Russell R, Hanson D, Williams RJ, Dunne JA (2015) Data from: Effects of spatial scale of sampling on food web structure. Dryad Digital Repository. http://dx.doi.org/10.5061/dryad.g1qr6"));
+        assertThat(firstLink.get("study_citation"), is("Wood SA, Russell R, Hanson D, Williams RJ, Dunne JA (2015) Effects of spatial scale of sampling on food web structure. Ecology and Evolution, online in advance of print. http://dx.doi.org/10.1002/ece3.1640"));
+        assertThat(firstLink.get("study_doi"), is("doi:10.1002/ece3.1640"));
+        assertThat(firstLink.get("study_url"), is("http://dx.doi.org/10.1002/ece3.1640"));
+        assertThat(firstLink.get("locality_name"), is("Sanak Island, Alaska, USA"));
+        assertThat(firstLink.get("locality_id"), is("GEONAMES:5873327"));
+        assertThat(firstLink.get("longitude"), is("-162.70889"));
+        assertThat(firstLink.get("latitude"), is("54.42972"));
+        assertThat(firstLink.get("interaction_type_id"), is("RO:0002439"));
+        assertThat(firstLink.get("interaction_type_name"), is("preysOn"));
+    }
+
+    private String firstFewLines() {
+        return "\"WebID\",\"WebScale\",\"WebUnit\",\"PredTSN\",\"PreyTSN\",\"PredName\",\"PreyName\"\n" +
+                "9,\"T\",\"22\",\"93294\",\"10824\",\"Amphipoda\",\"Pilayella littoralis\"\n" +
+                "9,\"T\",\"22\",\"san267\",\"2286\",\"Phytoplankton complex\",\"Bacillariophyta\"\n" +
+                "9,\"T\",\"22\",\"93294\",\"11334\",\"Amphipoda\",\"Fucus\"\n" +
+                "9,\"T\",\"22\",\"70395\",\"11334\",\"Littorina\",\"Fucus\"\n" +
+                "9,\"T\",\"22\",\"92283\",\"11334\",\"Sphaeromatidae\",\"Fucus\"\n";
+    }
+}
