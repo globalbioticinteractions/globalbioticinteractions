f7a052300d9ba0da10ebb5dc6aaf479ac33dd3af
Jorrit Poelen <jhpoelen@gmail.com>
2015-09-10 16:21:50 -0700

    fix build number stripper

--- .travis.version_if_tag.sh
+++ .travis.version_if_tag.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [ -n "$TRAVIS_TAG" ]; then
-  VERSION=${TRAVIS_TAG}//[^0-9.]/}.$TRAVIS_BUILD_NUMBER
+  VERSION=${TRAVIS_TAG//[^0-9.]/}.$TRAVIS_BUILD_NUMBER
   mvn -pl eol-globi-parent versions:set -DnewVersion=$VERSION
 fi
