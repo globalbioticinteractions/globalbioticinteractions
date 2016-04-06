17b9da56f4a6f41413ae69b86a441b770feae141
Jorrit Poelen <jhpoelen@gmail.com>
2016-04-05 18:06:26 -0700

    fix test error

--- eol-globi-taxon-resolver/src/test/java/org/eol/globi/taxon/NCBIServiceTest.java
+++ eol-globi-taxon-resolver/src/test/java/org/eol/globi/taxon/NCBIServiceTest.java
@@ -66,7 +66,7 @@ public class NCBIServiceTest {
     }
 
     @Test
-    public void lookupPathByUnmatchedId() throws PropertyEnricherException {
+    public void lookupPathByPreviouslyUnmatchedId() throws PropertyEnricherException {
         PropertyEnricher enricher = new NCBIService();
         HashMap<String, String> props = new HashMap<String, String>() {{
             put(PropertyAndValueDictionary.EXTERNAL_ID, "NCBI:235106");
@@ -74,7 +74,7 @@ public class NCBIServiceTest {
         Map<String, String> enrich = enricher.enrich(props);
         assertThat(enrich.get(PropertyAndValueDictionary.NAME), is("Influenza A virus (A/Taiwan/0562/1995(H1N1))"));
         assertThat(enrich.get(PropertyAndValueDictionary.PATH), containsString("Influenzavirus"));
-        assertThat(enrich.get(PropertyAndValueDictionary.EXTERNAL_ID), is(nullValue()));
+        assertThat(enrich.get(PropertyAndValueDictionary.EXTERNAL_ID), is("NCBI:235106"));
     }
 
 
