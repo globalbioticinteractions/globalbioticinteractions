60e5056a6a77e6dd2c0cb0bd262aeb0629bf3cc7
Jorrit Poelen <jhpoelen@gmail.com>
2016-04-06 14:27:30 -0700

    ensure to populate locality

--- eol-globi-lib/src/main/java/org/eol/globi/domain/LocationUtil.java
+++ eol-globi-lib/src/main/java/org/eol/globi/domain/LocationUtil.java
@@ -1,10 +1,12 @@
 package org.eol.globi.domain;
 
 public class LocationUtil {
-    public static Location fromLocation(Location location) {
-        return new LocationImpl(location.getLatitude(),
-                location.getLongitude(),
-                location.getAltitude(),
-                location.getFootprintWKT());
+    public static Location fromLocation(Location fromLocation) {
+        final LocationImpl toLocation = new LocationImpl(fromLocation.getLatitude(),
+                fromLocation.getLongitude(),
+                fromLocation.getAltitude(),
+                fromLocation.getFootprintWKT());
+        toLocation.setLocality(fromLocation.getLocality());
+        return toLocation;
     }
 }
