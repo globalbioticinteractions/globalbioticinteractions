cf04682760eff2abe65f68a73a583ea8c395bfa3
jhpoelen <jhpoelen@gmail.com>
2016-12-22 12:02:16 -0800

    fix exception signature

--- eol-globi-data-tool/src/test/java/org/eol/globi/tool/GitHubRepoCheckTest.java
+++ eol-globi-data-tool/src/test/java/org/eol/globi/tool/GitHubRepoCheckTest.java
@@ -1,19 +1,17 @@
 package org.eol.globi.tool;
 
-import org.apache.commons.cli.ParseException;
 import org.eol.globi.data.StudyImporterException;
-import org.eol.globi.service.GitHubUtil;
+import org.eol.globi.service.DatasetFinderException;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 
 public class GitHubRepoCheckTest {
 
     @Test
-    public void doSingleImportArgs() throws ParseException, StudyImporterException, IOException, URISyntaxException {
+    public void doSingleImportArgs() throws DatasetFinderException, StudyImporterException, IOException {
         final String repoName = "globalbioticinteractions/template-dataset";
-        GitHubRepoCheck.main(new String[]{repoName, GitHubUtil.getBaseUrlMaster(repoName)});
+        GitHubRepoCheck.main(new String[]{repoName});
     }
 
 }
