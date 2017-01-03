d069a1977ecc01cd2bb49ab260871c8f35e55933
jhpoelen <jhpoelen@gmail.com>
2017-01-03 15:26:52 -0800

    propagate exceptions of github importers

--- eol-globi-data-sources/src/main/java/org/eol/globi/data/StudyImporterForGitHubData.java
+++ eol-globi-data-sources/src/main/java/org/eol/globi/data/StudyImporterForGitHubData.java
@@ -1,5 +1,6 @@
 package org.eol.globi.data;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.eol.globi.domain.Study;
@@ -13,6 +14,7 @@ import org.eol.globi.service.DatasetFinderProxy;
 import org.eol.globi.service.DatasetFinderZenodo;
 import org.eol.globi.service.GitHubImporterFactory;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -35,21 +37,32 @@ public class StudyImporterForGitHubData extends BaseStudyImporter {
             throw new StudyImporterException("failed to discover datasets", e);
         }
 
+
+        List<String> repositoriesWithIssues = new ArrayList<>();
         for (String repository : repositories) {
-            importData(repository);
+            try {
+                importData(repository);
+            } catch (StudyImporterException e) {
+                repositoriesWithIssues.add(repository);
+            }
         }
 
+        if (repositoriesWithIssues.size() > 0) {
+            throw new StudyImporterException("failed to import one or more repositories: [" + StringUtils.join(repositoriesWithIssues, ", ") + "]");
+        }
         return null;
     }
 
-    public void importData(String repository) {
+    public void importData(String repository) throws StudyImporterException {
         try {
             LOG.info("importing github repo [" + repository + "]...");
             Dataset dataset = DatasetFactory.datasetFor(repository, getDatasetFinder());
             importData(dataset);
             LOG.info("importing github repo [" + repository + "] done.");
         } catch (StudyImporterException | DatasetFinderException ex) {
-            LOG.error("failed to import data from repo [" + repository + "]", ex);
+            String msg = "failed to import data from repo [" + repository + "]";
+            LOG.error(msg, ex);
+            throw new StudyImporterException(msg, ex);
         }
     }
 
