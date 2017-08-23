9d0c521d1b1e468b5f1d129d8478202f2fb16bec
jhpoelen <jhpoelen@xs4all.nl>
2017-08-23 09:52:24 -0700

    lib should not rely on neo4j speak

--- eol-globi-data-tool/src/main/java/org/eol/globi/tool/IndexInteractions.java
+++ eol-globi-data-tool/src/main/java/org/eol/globi/tool/IndexInteractions.java
@@ -2,33 +2,11 @@ package org.eol.globi.tool;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.eol.globi.data.NodeFactory;
-import org.eol.globi.data.NodeFactoryNeo4j;
-import org.eol.globi.data.NodeFactoryWithDatasetContext;
-import org.eol.globi.domain.DatasetNode;
-import org.eol.globi.domain.InteractType;
-import org.eol.globi.domain.InteractionNode;
-import org.eol.globi.domain.PropertyAndValueDictionary;
-import org.eol.globi.domain.RelTypes;
-import org.eol.globi.domain.StudyNode;
-import org.eol.globi.util.CypherUtil;
 import org.eol.globi.util.InteractUtil;
-import org.eol.globi.util.NodeUtil;
 import org.neo4j.cypher.javacompat.ExecutionEngine;
 import org.neo4j.cypher.javacompat.ExecutionResult;
-import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Relationship;
-import org.neo4j.graphdb.RelationshipType;
-import org.neo4j.graphdb.Transaction;
-import org.neo4j.graphdb.index.Index;
-import org.neo4j.graphdb.index.IndexHits;
 import org.neo4j.helpers.collection.MapUtil;
-
-import java.util.Iterator;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class IndexInteractions implements Linker {
     private static final Log LOG = LogFactory.getLog(IndexInteractions.class);
