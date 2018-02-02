448305a3f3103e4f89571bd408c7d3a729e280d1
jhpoelen <jhpoelen@xs4all.nl>
2018-02-02 10:59:52 -0800

    simplify

--- eol-globi-neo4j-index/src/main/java/org/eol/globi/tool/ImageLinker.java
+++ eol-globi-neo4j-index/src/main/java/org/eol/globi/tool/ImageLinker.java
@@ -1,21 +1,11 @@
 package org.eol.globi.tool;
 
-import org.apache.commons.cli.BasicParser;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.eol.globi.data.StudyImporterException;
-import org.eol.globi.db.GraphService;
 import org.eol.globi.domain.TaxonImage;
 import org.eol.globi.service.EOLTaxonImageService;
 import org.eol.globi.util.CSVTSVUtil;
-import org.eol.globi.util.HttpUtil;
 import org.neo4j.cypher.javacompat.ExecutionEngine;
 import org.neo4j.cypher.javacompat.ExecutionResult;
 import org.neo4j.graphdb.GraphDatabaseService;
@@ -28,8 +18,6 @@ import java.util.Map;
 
 public class ImageLinker implements Linker {
     private static final Log LOG = LogFactory.getLog(ImageLinker.class);
-    public static final String OPTION_HELP = "h";
-    public static final String USE_NEO4J_SOURCE = "skipImport";
 
     private final PrintStream out;
     private final GraphDatabaseService graphDb;
@@ -37,41 +25,6 @@ public class ImageLinker implements Linker {
     public ImageLinker(GraphDatabaseService graphService, PrintStream out) {
         this.graphDb = graphService;
         this.out = out;
-    }
-
-    public static void main(final String[] args) throws StudyImporterException, ParseException {
-        CommandLine cmdLine = parseOptions(args);
-        if (cmdLine.hasOption(OPTION_HELP)) {
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("java -jar eol-globi-data-tool-[VERSION]-jar-with-dependencies.jar", getOptions());
-        } else {
-            ImageLinker.run(cmdLine);
-        }
-    }
-
-
-    protected static CommandLine parseOptions(String[] args) throws ParseException {
-        CommandLineParser parser = new BasicParser();
-        return parser.parse(getOptions(), args);
-    }
-
-    private static Options getOptions() {
-        Options options = new Options();
-        options.addOption(USE_NEO4J_SOURCE, false, "taxon ids from GloBI neo4j");
-        Option helpOpt = new Option(OPTION_HELP, "help", false, "print this help information");
-        options.addOption(helpOpt);
-        return options;
-    }
-
-    private static void run(CommandLine cmdLine)  {
-        final GraphDatabaseService graphService = GraphService.getGraphService("./");
-        try {
-            new ImageLinker(graphService, System.out).link();
-        } finally {
-            graphService.shutdown();
-            HttpUtil.shutdown();
-        }
-
     }
 
     @Override
