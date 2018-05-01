5366eaf48b9a09b63c20c49f6f6e541479f19081
jhpoelen <jhpoelen@xs4all.nl>
2018-05-01 10:18:16 -0700

    nanopub moved to elton

--- eol-globi-neo4j-index/src/main/java/org/eol/globi/tool/NanopubOutputStreamFactory.java
+++
@@ -1,9 +0,0 @@
-package org.eol.globi.tool;
-
-import org.nanopub.Nanopub;
-
-import java.io.OutputStream;
-
-public interface NanopubOutputStreamFactory {
-    OutputStream outputStreamFor(Nanopub nanopub);
-}
