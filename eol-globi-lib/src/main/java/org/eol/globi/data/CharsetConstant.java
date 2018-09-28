e8e5613f6a3e5ac6c15db6619aadd0b8209708b9
jhpoelen <jhpoelen@xs4all.nl>
2018-09-27 17:05:04 -0700

    attempt to prune heavy dependencies

--- eol-globi-lib/src/main/java/org/eol/globi/data/CharsetConstant.java
+++ eol-globi-lib/src/main/java/org/eol/globi/data/CharsetConstant.java
@@ -1,7 +1,9 @@
 package org.eol.globi.data;
 
+import java.nio.charset.StandardCharsets;
+
 public class CharsetConstant {
-    public static final String UTF8 = "UTF-8";
+    public static final String UTF8 = StandardCharsets.UTF_8.name();
     public static final String SEPARATOR_CHAR = "|";
     public static final String SEPARATOR = " " + SEPARATOR_CHAR + " ";
     public static final String LANG_SEPARATOR_CHAR = "@";
