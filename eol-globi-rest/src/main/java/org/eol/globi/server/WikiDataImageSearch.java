901a4b0ae5462375d86e153004d2efd93e6b54eb
jhpoelen <jhpoelen@xs4all.nl>
2018-11-19 21:57:35 -0800

    add logging; fix wikidata query; set wd taxon page as info url

--- eol-globi-rest/src/main/java/org/eol/globi/server/WikiDataImageSearch.java
+++ eol-globi-rest/src/main/java/org/eol/globi/server/WikiDataImageSearch.java
@@ -1,18 +1,22 @@
 package org.eol.globi.server;
 
-import org.apache.http.client.HttpClient;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.http.client.methods.HttpGet;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.eol.globi.domain.TaxonImage;
 import org.eol.globi.domain.TaxonomyProvider;
 import org.eol.globi.service.ImageSearch;
+import org.eol.globi.util.ExternalIdUtil;
 import org.eol.globi.util.HttpUtil;
 import org.springframework.util.StringUtils;
 
 import java.io.IOException;
 
 public class WikiDataImageSearch implements ImageSearch {
+
+    private static Log LOG = LogFactory.getLog(WikiDataImageSearch.class);
 
     @Override
     public TaxonImage lookupImageForExternalId(String externalId) throws IOException {
@@ -24,10 +28,11 @@ public class WikiDataImageSearch implements ImageSearch {
     private TaxonImage requestImage(String externalId) throws IOException {
         TaxonImage taxonImage;
         taxonImage = new TaxonImage();
-        String wikiDataId = StringUtils.replace(externalId, TaxonomyProvider.WIKIDATA.getIdPrefix(), "Q");
+        String wikiDataId = StringUtils.replace(externalId, TaxonomyProvider.WIKIDATA.getIdPrefix(), "");
         String url = "https://query.wikidata.org/sparql?query=SELECT%20%3Fitem%20%3Fpic%20WHERE%20%7B%20%3Fitem%20wdt%3AP31%20wd%3A"
                 + wikiDataId
                 + "%20.%20%3Fitem%20wdt%3AP18%20%3Fpic%20%7D%20limit%201";
+        LOG.info("requesting image [" + url + "]");
         HttpGet httpGet = HttpUtil.httpGetJson(url);
         String jsonString = HttpUtil.executeAndRelease(httpGet, HttpUtil.getFailFastHttpClient());
         JsonNode jsonNode = new ObjectMapper().readTree(jsonString);
@@ -36,10 +41,7 @@ public class WikiDataImageSearch implements ImageSearch {
             if (results.has("bindings")) {
                 JsonNode bindings = results.get("bindings");
                 for (JsonNode binding : bindings) {
-                    JsonNode info = binding.get("item");
-                    if (info.has("value")) {
-                        taxonImage.setInfoURL(info.get("value").asText());
-                    }
+                    taxonImage.setInfoURL(ExternalIdUtil.urlForExternalId(externalId));
                     JsonNode pic = binding.get("pic");
                     if (pic.has("value")) {
                         taxonImage.setThumbnailURL(pic.get("value").asText() + "?width=100");
