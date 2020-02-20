8594a2167a9fa692227095e9d759eef2ad92be27
jhpoelen <jhpoelen@xs4all.nl>
2020-02-20 12:47:50 -0800

    remove unused import

--- eol-globi-lib/src/test/java/org/eol/globi/util/InteractTypeMapperFactoryForROTest.java
+++ eol-globi-lib/src/test/java/org/eol/globi/util/InteractTypeMapperFactoryForROTest.java
@@ -1,6 +1,5 @@
 package org.eol.globi.util;
 
-import javafx.beans.property.Property;
 import org.apache.commons.lang3.StringUtils;
 import org.eol.globi.domain.InteractType;
 import org.eol.globi.domain.PropertyAndValueDictionary;
