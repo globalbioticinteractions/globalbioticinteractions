80f13586b5c3013bd7032635eed2d1b56662d7c8
jhpoelen <jhpoelen@xs4all.nl>
2020-04-08 18:18:40 -0700

    remove usage of deprecated method

--- eol-globi-neo4j-index/src/main/java/org/eol/globi/db/GraphService.java
+++ eol-globi-neo4j-index/src/main/java/org/eol/globi/db/GraphService.java
@@ -3,9 +3,8 @@ package org.eol.globi.db;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.neo4j.graphdb.factory.GraphDatabaseSettings;
-import org.neo4j.helpers.collection.MapUtil;
 
-import java.util.Map;
+import java.io.File;
 
 public abstract class GraphService {
 
@@ -23,7 +22,7 @@ public abstract class GraphService {
         System.out.println("neo4j starting using [" + storePath + "]...");
 
         final GraphDatabaseService graphService = new GraphDatabaseFactory()
-                .newEmbeddedDatabaseBuilder(storePath)
+                .newEmbeddedDatabaseBuilder(new File(storePath))
                 .setConfig(GraphDatabaseSettings.keep_logical_logs, "150M size")
                 .setConfig(GraphDatabaseSettings.logical_log_rotation_threshold, "50M")
                 .setConfig(GraphDatabaseSettings.check_point_interval_tx, "1000")
