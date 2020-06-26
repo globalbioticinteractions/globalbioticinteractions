e4a17b5efd78a1cc8a8e4362027574a2125ca4c2
jhpoelen <jhpoelen@xs4all.nl>
2020-06-26 13:45:45 -0700

    deprecate exposing local URI; favor location agnostic streams instead.

--- eol-globi-data-sources/src/test/java/org/eol/globi/service/DatasetTest.java
+++ eol-globi-data-sources/src/test/java/org/eol/globi/service/DatasetTest.java
@@ -4,20 +4,42 @@ import org.globalbioticinteractions.dataset.Dataset;
 import org.globalbioticinteractions.dataset.DatasetImpl;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 public class DatasetTest {
 
     @Test
-    public void resourceURL() throws IOException {
-        Dataset dataset = new DatasetImpl("some/namespace", URI.create("http://example.com"), inStream -> inStream);
-        assertThat(URI.create("http://otherexample.com/bla"), is(dataset.getLocalURI(URI.create("http://otherexample.com/bla"))));
-        assertThat(URI.create("http://example.com/someResource"), is(dataset.getLocalURI(URI.create("/someResource"))));
-        assertThat(URI.create("http://example.com/someResource"), is(dataset.getLocalURI(URI.create("someResource"))));
+    public void resourceURL() throws IOException, URISyntaxException {
+        URL resource = getClass().getResource("archive.zip");
+        File archive = new File(resource.toURI());
+        assertThat(archive.exists(), is(true));
+
+        URI parentDir = archive.getParentFile().toURI();
+
+        Dataset dataset = new DatasetImpl("some/namespace", parentDir, inStream -> inStream);
+        assertThat(dataset.retrieve(resource.toURI()), is(notNullValue()));
+        assertThat(dataset.retrieve(URI.create("archive.zip")), is(notNullValue()));
+        assertThat(dataset.retrieve(URI.create("/archive.zip")), is(notNullValue()));
+    }
+
+    @Test(expected = IOException.class)
+    public void unknownResourceURL() throws IOException, URISyntaxException {
+        URL resource = getClass().getResource("archive.zip");
+        File archive = new File(resource.toURI());
+        assertThat(archive.exists(), is(true));
+
+        URI parentDir = archive.getParentFile().toURI();
+
+        Dataset dataset = new DatasetImpl("some/namespace", parentDir, inStream -> inStream);
+        assertThat(dataset.retrieve(URI.create("archivezz.zip")), is(notNullValue()));
     }
 
 
