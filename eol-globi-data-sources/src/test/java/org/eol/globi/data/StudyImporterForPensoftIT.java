8c60527d03089c3b9045326b334fed0cf91ee601
jhpoelen <jhpoelen@xs4all.nl>
2020-07-14 14:54:50 -0700

    separate unit tests from integration tests

--- eol-globi-data-sources/src/test/java/org/eol/globi/data/StudyImporterForPensoftIT.java
+++ eol-globi-data-sources/src/test/java/org/eol/globi/data/StudyImporterForPensoftIT.java
@@ -40,6 +40,9 @@ import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 import static junit.framework.TestCase.assertNotNull;
+import static org.eol.globi.data.StudyImporterForPensoftTest.getResourceServiceTest;
+import static org.eol.globi.data.StudyImporterForPensoftTest.getTableObj;
+import static org.eol.globi.data.StudyImporterForPensoftTest.parseRowsAndEnrich;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.hasItem;
@@ -79,4 +82,147 @@ public class StudyImporterForPensoftIT {
         assertThat(links.get(0), hasEntry("referenceDoi", "10.3897/zookeys.306.5455"));
         assertThat(links.get(0), hasEntry("referenceCitation", "Identification of the terebrantian thrips (Insecta, Thysanoptera) associated with cultivated plants in Java, Indonesia. http://openbiodiv.net/D37E8D1A-221B-FFA6-FFE7-4458FFA0FFC2. 10.3897/zookeys.306.5455"));
     }
+
+    @Test
+    public void retrieveCitation() throws IOException {
+        String citation = StudyImporterForPensoft.findCitationByDoi("10.3897/zookeys.306.5455", getResourceServiceTest());
+        assertThat(citation, is("Identification of the terebrantian thrips (Insecta, Thysanoptera) associated with cultivated plants in Java, Indonesia. http://openbiodiv.net/D37E8D1A-221B-FFA6-FFE7-4458FFA0FFC2. https://doi.org/10.3897/zookeys.306.5455"));
+    }
+
+    @Test
+    public void retrieveTaxonFamily() throws IOException {
+        Taxon taxon = StudyImporterForPensoft.retrieveTaxonHierarchyById("4B689A17-2541-4F5F-A896-6F0C2EEA3FB4", getResourceServiceTest());
+        assertThat(taxon.getName(), is("Acanthaceae"));
+        assertThat(taxon.getRank(), is("family"));
+        assertThat(taxon.getExternalId(), is("http://openbiodiv.net/4B689A17-2541-4F5F-A896-6F0C2EEA3FB4"));
+        assertThat(taxon.getPath(), is("Plantae | Tracheophyta | Magnoliopsida | Lamiales | Acanthaceae"));
+        assertThat(taxon.getPathNames(), is("kingdom | phylum | class | order | family"));
+    }
+
+    @Test
+    public void retrieveTaxonSpecies() throws IOException {
+        Taxon taxon = StudyImporterForPensoft.retrieveTaxonHierarchyById("6A54156A-BE5C-44D7-A9E3-3902DA4CCFAC", getResourceServiceTest());
+        assertThat(taxon.getName(), is("Copidothrips octarticulatus"));
+        assertThat(taxon.getRank(), is(nullValue()));
+        assertThat(taxon.getExternalId(), is("http://openbiodiv.net/6A54156A-BE5C-44D7-A9E3-3902DA4CCFAC"));
+        assertThat(taxon.getPath(), is("Copidothrips octarticulatus"));
+        assertThat(taxon.getPathNames(), is(""));
+    }
+
+    @Test
+    public void retrieveTaxonSpecies2() throws IOException {
+        Taxon taxon = StudyImporterForPensoft.retrieveTaxonHierarchyById("22A7F215-829B-458A-AEBB-39FFEA6D4A91", getResourceServiceTest());
+        assertThat(taxon.getName(), is("Bolacothrips striatopennatus"));
+        assertThat(taxon.getRank(), is("species"));
+        assertThat(taxon.getExternalId(), is("http://openbiodiv.net/22A7F215-829B-458A-AEBB-39FFEA6D4A91"));
+        assertThat(taxon.getPath(), is("Animalia | Arthropoda | Insecta | Thysanoptera | Thripidae | Bolacothrips | Bolacothrips striatopennatus"));
+        assertThat(taxon.getPathNames(), is("kingdom | phylum | class | order | family | genus | species"));
+    }
+
+    @Test
+    public void singleTableHostInColumnHeader() throws IOException, TermLookupServiceException, StudyImporterException {
+
+        final JsonNode tableObj = getTableObj();
+
+        assertNotNull(tableObj);
+
+        tableObj.get("article_doi");
+        final JsonNode annotations = tableObj.get("annotations");
+        for (JsonNode annotation : annotations) {
+            final InteractTypeMapper interactTypeMapper =
+                    new InteractTypeMapperFactoryImpl().create();
+            if (annotation.has("id")) {
+                final InteractType interactType = interactTypeMapper.getInteractType(annotation.get("id").asText());
+            }
+            if (annotation.has("context")) {
+                String verbatimInteraction = annotation.get("context").asText();
+            }
+            annotation.get("row");
+            annotation.get("column");
+            annotation.get("possition");
+
+
+        }
+
+        tableObj.get("caption");
+
+
+        List<Map<String, String>> rowValues = new ArrayList<>();
+        InteractionListener listener = new InteractionListener() {
+            @Override
+            public void newLink(Map<String, String> link) throws StudyImporterException {
+                rowValues.add(link);
+            }
+        };
+
+        parseRowsAndEnrich(tableObj, listener, getResourceServiceTest());
+
+    }
+
+    @Test
+    public void parseTableContent() throws IOException, TermLookupServiceException, StudyImporterException {
+
+        final JsonNode tableObj = getTableObj();
+
+        List<Map<String, String>> rowValues = new ArrayList<>();
+        InteractionListener listener = new InteractionListener() {
+            @Override
+            public void newLink(Map<String, String> link) throws StudyImporterException {
+                rowValues.add(new TreeMap<>(link));
+            }
+        };
+
+        parseRowsAndEnrich(tableObj, listener, getResourceServiceTest());
+
+        assertThat(rowValues.size(), is(121));
+        assertThat(rowValues.get(0), hasEntry("Family Name", "Acanthaceae"));
+        assertThat(rowValues.get(0), hasEntry("Family Name_taxon_id", "http://openbiodiv.net/4B689A17-2541-4F5F-A896-6F0C2EEA3FB4"));
+        assertThat(rowValues.get(0), hasEntry("Family Name_taxon_name", "Acanthaceae"));
+        assertThat(rowValues.get(0), hasEntry("Host Plant", "Ruellia sp."));
+        assertThat(rowValues.get(0), hasEntry("Host Plant_taxon_id", "http://openbiodiv.net/56F59D49-725E-4BF7-8A6D-1B1A7A721231"));
+        assertThat(rowValues.get(0), hasEntry("Host Plant_taxon_name", "Ruellia"));
+        assertThat(rowValues.get(0), hasEntry("Thrips species", "Copidothrips octarticulatus<br/> Thrips parvispinus"));
+        assertThat(rowValues.get(0), hasEntry("Thrips species_taxon_id", "http://openbiodiv.net/6A54156A-BE5C-44D7-A9E3-3902DA4CCFAC"));
+        assertThat(rowValues.get(0), hasEntry("Thrips species_taxon_name", "Copidothrips octarticulatus"));
+
+        assertThat(rowValues.get(1), hasEntry("Family Name", "Acanthaceae"));
+        assertThat(rowValues.get(1), hasEntry("Family Name_taxon_id", "http://openbiodiv.net/4B689A17-2541-4F5F-A896-6F0C2EEA3FB4"));
+        assertThat(rowValues.get(1), hasEntry("Family Name_taxon_name", "Acanthaceae"));
+
+    }
+
+    @Test
+    public void parseTableContentWithRowSpan() throws IOException, TermLookupServiceException, StudyImporterException {
+
+        final String tableContent = IOUtils.toString(getClass().getResourceAsStream("pensoft/table-with-rowspan.html"), StandardCharsets.UTF_8);
+
+        final ObjectNode tableObj = new ObjectMapper().createObjectNode();
+        tableObj.put("table_id", "<http://openbiodiv.net/FB706B4E-BAC2-4432-AD28-48063E7753E4>");
+        tableObj.put("caption", "a caption");
+        tableObj.put("article_doi", "10.12/34");
+        tableObj.put("table_content", tableContent);
+
+        List<Map<String, String>> rowValues = new ArrayList<>();
+        InteractionListener listener = new InteractionListener() {
+            @Override
+            public void newLink(Map<String, String> link) throws StudyImporterException {
+                rowValues.add(new TreeMap<>(link));
+            }
+        };
+
+        parseRowsAndEnrich(tableObj, listener, getResourceServiceTest());
+
+        assertThat(rowValues.size(), is(8));
+        assertThat(rowValues.get(0), hasEntry("Family Name", "Apiaceae"));
+        assertThat(rowValues.get(0), hasEntry("Thrips species_taxon_name", "Thrips parvispinus"));
+        assertThat(rowValues.get(1), hasEntry("Family Name", "Apiaceae"));
+        assertThat(rowValues.get(1), hasEntry("Thrips species_taxon_name", "Thrips nigropilosus"));
+        assertThat(rowValues.get(2), hasEntry("Family Name", "Apiaceae"));
+        assertThat(rowValues.get(2), hasEntry("Thrips species_taxon_name", "Thrips parvispinus"));
+        assertThat(rowValues.get(7), hasEntry("Family Name", "Apocynaceae"));
+        assertThat(rowValues.get(7), hasEntry("Thrips species_taxon_name", "Thrips malloti"));
+
+    }
+
+
 }
--- eol-globi-data-sources/src/test/java/org/eol/globi/data/StudyImporterForPensoftTest.java
+++ eol-globi-data-sources/src/test/java/org/eol/globi/data/StudyImporterForPensoftTest.java
@@ -7,16 +7,12 @@ import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.node.ArrayNode;
 import org.codehaus.jackson.node.ObjectNode;
-import org.eol.globi.domain.InteractType;
-import org.eol.globi.domain.Taxon;
 import org.eol.globi.domain.Term;
 import org.eol.globi.domain.TermImpl;
 import org.eol.globi.service.ResourceService;
 import org.eol.globi.service.TermLookupServiceException;
 import org.eol.globi.tool.NullImportLogger;
 import org.eol.globi.util.HttpUtil;
-import org.eol.globi.util.InteractTypeMapper;
-import org.eol.globi.util.InteractTypeMapperFactoryImpl;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -35,13 +31,11 @@ import java.util.Map;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 
-import static junit.framework.TestCase.assertNotNull;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
 
 public class StudyImporterForPensoftTest {
 
@@ -78,13 +72,8 @@ public class StudyImporterForPensoftTest {
                 "}"));
     }
 
-    @Test
-    public void retrieveCitation() throws IOException {
-        String citation = StudyImporterForPensoft.findCitationByDoi("10.3897/zookeys.306.5455", getResourceServiceTest());
-        assertThat(citation, is("Identification of the terebrantian thrips (Insecta, Thysanoptera) associated with cultivated plants in Java, Indonesia. http://openbiodiv.net/D37E8D1A-221B-FFA6-FFE7-4458FFA0FFC2. https://doi.org/10.3897/zookeys.306.5455"));
-    }
 
-    public ResourceService getResourceServiceTest() {
+    public static ResourceService getResourceServiceTest() {
         return new ResourceService() {
 
             @Override
@@ -96,75 +85,7 @@ public class StudyImporterForPensoftTest {
         };
     }
 
-    @Test
-    public void retrieveTaxonFamily() throws IOException {
-        Taxon taxon = StudyImporterForPensoft.retrieveTaxonHierarchyById("4B689A17-2541-4F5F-A896-6F0C2EEA3FB4", getResourceServiceTest());
-        assertThat(taxon.getName(), is("Acanthaceae"));
-        assertThat(taxon.getRank(), is("family"));
-        assertThat(taxon.getExternalId(), is("http://openbiodiv.net/4B689A17-2541-4F5F-A896-6F0C2EEA3FB4"));
-        assertThat(taxon.getPath(), is("Plantae | Tracheophyta | Magnoliopsida | Lamiales | Acanthaceae"));
-        assertThat(taxon.getPathNames(), is("kingdom | phylum | class | order | family"));
-    }
 
-    @Test
-    public void retrieveTaxonSpecies() throws IOException {
-        Taxon taxon = StudyImporterForPensoft.retrieveTaxonHierarchyById("6A54156A-BE5C-44D7-A9E3-3902DA4CCFAC", getResourceServiceTest());
-        assertThat(taxon.getName(), is("Copidothrips octarticulatus"));
-        assertThat(taxon.getRank(), is(nullValue()));
-        assertThat(taxon.getExternalId(), is("http://openbiodiv.net/6A54156A-BE5C-44D7-A9E3-3902DA4CCFAC"));
-        assertThat(taxon.getPath(), is("Copidothrips octarticulatus"));
-        assertThat(taxon.getPathNames(), is(""));
-    }
-
-    @Test
-    public void retrieveTaxonSpecies2() throws IOException {
-        Taxon taxon = StudyImporterForPensoft.retrieveTaxonHierarchyById("22A7F215-829B-458A-AEBB-39FFEA6D4A91", getResourceServiceTest());
-        assertThat(taxon.getName(), is("Bolacothrips striatopennatus"));
-        assertThat(taxon.getRank(), is("species"));
-        assertThat(taxon.getExternalId(), is("http://openbiodiv.net/22A7F215-829B-458A-AEBB-39FFEA6D4A91"));
-        assertThat(taxon.getPath(), is("Animalia | Arthropoda | Insecta | Thysanoptera | Thripidae | Bolacothrips | Bolacothrips striatopennatus"));
-        assertThat(taxon.getPathNames(), is("kingdom | phylum | class | order | family | genus | species"));
-    }
-
-    @Test
-    public void singleTableHostInColumnHeader() throws IOException, TermLookupServiceException, StudyImporterException {
-
-        final JsonNode tableObj = getTableObj();
-
-        assertNotNull(tableObj);
-
-        tableObj.get("article_doi");
-        final JsonNode annotations = tableObj.get("annotations");
-        for (JsonNode annotation : annotations) {
-            final InteractTypeMapper interactTypeMapper =
-                    new InteractTypeMapperFactoryImpl().create();
-            if (annotation.has("id")) {
-                final InteractType interactType = interactTypeMapper.getInteractType(annotation.get("id").asText());
-            }
-            if (annotation.has("context")) {
-                String verbatimInteraction = annotation.get("context").asText();
-            }
-            annotation.get("row");
-            annotation.get("column");
-            annotation.get("possition");
-
-
-        }
-
-        tableObj.get("caption");
-
-
-        List<Map<String, String>> rowValues = new ArrayList<>();
-        InteractionListener listener = new InteractionListener() {
-            @Override
-            public void newLink(Map<String, String> link) throws StudyImporterException {
-                rowValues.add(link);
-            }
-        };
-
-        parseRowsAndEnrich(tableObj, listener, getResourceServiceTest());
-
-    }
 
     @Test
     public void handleRowSpan() throws IOException, TermLookupServiceException, StudyImporterException {
@@ -210,73 +131,10 @@ public class StudyImporterForPensoftTest {
         assertThat(rowValue3, hasEntry("one", "Apocynaceae"));
     }
 
-    @Test
-    public void parseTableContent() throws IOException, TermLookupServiceException, StudyImporterException {
 
-        final JsonNode tableObj = getTableObj();
 
-        List<Map<String, String>> rowValues = new ArrayList<>();
-        InteractionListener listener = new InteractionListener() {
-            @Override
-            public void newLink(Map<String, String> link) throws StudyImporterException {
-                rowValues.add(new TreeMap<>(link));
-            }
-        };
-
-        parseRowsAndEnrich(tableObj, listener, getResourceServiceTest());
-
-        assertThat(rowValues.size(), is(121));
-        assertThat(rowValues.get(0), hasEntry("Family Name", "Acanthaceae"));
-        assertThat(rowValues.get(0), hasEntry("Family Name_taxon_id", "http://openbiodiv.net/4B689A17-2541-4F5F-A896-6F0C2EEA3FB4"));
-        assertThat(rowValues.get(0), hasEntry("Family Name_taxon_name", "Acanthaceae"));
-        assertThat(rowValues.get(0), hasEntry("Host Plant", "Ruellia sp."));
-        assertThat(rowValues.get(0), hasEntry("Host Plant_taxon_id", "http://openbiodiv.net/56F59D49-725E-4BF7-8A6D-1B1A7A721231"));
-        assertThat(rowValues.get(0), hasEntry("Host Plant_taxon_name", "Ruellia"));
-        assertThat(rowValues.get(0), hasEntry("Thrips species", "Copidothrips octarticulatus<br/> Thrips parvispinus"));
-        assertThat(rowValues.get(0), hasEntry("Thrips species_taxon_id", "http://openbiodiv.net/6A54156A-BE5C-44D7-A9E3-3902DA4CCFAC"));
-        assertThat(rowValues.get(0), hasEntry("Thrips species_taxon_name", "Copidothrips octarticulatus"));
-
-        assertThat(rowValues.get(1), hasEntry("Family Name", "Acanthaceae"));
-        assertThat(rowValues.get(1), hasEntry("Family Name_taxon_id", "http://openbiodiv.net/4B689A17-2541-4F5F-A896-6F0C2EEA3FB4"));
-        assertThat(rowValues.get(1), hasEntry("Family Name_taxon_name", "Acanthaceae"));
-
-    }
-
-    @Test
-    public void parseTableContentWithRowSpan() throws IOException, TermLookupServiceException, StudyImporterException {
-
-        final String tableContent = IOUtils.toString(getClass().getResourceAsStream("pensoft/table-with-rowspan.html"), StandardCharsets.UTF_8);
-
-        final ObjectNode tableObj = new ObjectMapper().createObjectNode();
-        tableObj.put("table_id", "<http://openbiodiv.net/FB706B4E-BAC2-4432-AD28-48063E7753E4>");
-        tableObj.put("caption", "a caption");
-        tableObj.put("article_doi", "10.12/34");
-        tableObj.put("table_content", tableContent);
-
-        List<Map<String, String>> rowValues = new ArrayList<>();
-        InteractionListener listener = new InteractionListener() {
-            @Override
-            public void newLink(Map<String, String> link) throws StudyImporterException {
-                rowValues.add(new TreeMap<>(link));
-            }
-        };
-
-        parseRowsAndEnrich(tableObj, listener, getResourceServiceTest());
-
-        assertThat(rowValues.size(), is(8));
-        assertThat(rowValues.get(0), hasEntry("Family Name", "Apiaceae"));
-        assertThat(rowValues.get(0), hasEntry("Thrips species_taxon_name", "Thrips parvispinus"));
-        assertThat(rowValues.get(1), hasEntry("Family Name", "Apiaceae"));
-        assertThat(rowValues.get(1), hasEntry("Thrips species_taxon_name", "Thrips nigropilosus"));
-        assertThat(rowValues.get(2), hasEntry("Family Name", "Apiaceae"));
-        assertThat(rowValues.get(2), hasEntry("Thrips species_taxon_name", "Thrips parvispinus"));
-        assertThat(rowValues.get(7), hasEntry("Family Name", "Apocynaceae"));
-        assertThat(rowValues.get(7), hasEntry("Thrips species_taxon_name", "Thrips malloti"));
-
-    }
-
-    public JsonNode getTableObj() throws IOException {
-        final InputStream is = getClass().getResourceAsStream("pensoft/annotated-table.json");
+    public static JsonNode getTableObj() throws IOException {
+        final InputStream is = StudyImporterForPensoftTest.class.getResourceAsStream("pensoft/annotated-table.json");
         return new ObjectMapper().readTree(is);
     }
 
@@ -352,7 +210,6 @@ public class StudyImporterForPensoftTest {
 
         StudyImporterForPensoft.expandRows(termPairs, new TestPermutationListener(links), StudyImporterForPensoft.distinctKeys(termPairs));
 
-        links.forEach(System.out::println);
         assertThat(links.size(), is(4));
 
         final Map<String, String> expectedItem1 = new TreeMap<String, String>() {{
