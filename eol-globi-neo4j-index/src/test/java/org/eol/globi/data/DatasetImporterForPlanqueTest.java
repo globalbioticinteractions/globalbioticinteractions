dc180d955ffc6025ba4a5d7c0daf2a7b22482b79
jhpoelen <jhpoelen@xs4all.nl>
2020-10-13 11:30:31 -0700

    organize imports

--- eol-globi-neo4j-index/src/test/java/org/eol/globi/data/DatasetImporterForPlanqueTest.java
+++ eol-globi-neo4j-index/src/test/java/org/eol/globi/data/DatasetImporterForPlanqueTest.java
@@ -1,21 +1,8 @@
 package org.eol.globi.data;
 
-import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.eol.globi.domain.LogContext;
-import org.eol.globi.domain.StudyNode;
-import org.eol.globi.tool.NullImportLogger;
-import org.eol.globi.util.NodeUtil;
-import org.globalbioticinteractions.dataset.DatasetImpl;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertThat;
 
 public class DatasetImporterForPlanqueTest extends GraphDBTestCase {
