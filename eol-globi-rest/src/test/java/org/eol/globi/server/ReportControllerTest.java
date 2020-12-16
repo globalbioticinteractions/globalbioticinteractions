9ea1691fa5594075a5c1c794400fb7fbc13b3fcf
jhpoelen <jhpoelen@xs4all.nl>
2020-12-15 16:13:50 -0800

    reformat query tests to increase readability

--- eol-globi-rest/src/test/java/org/eol/globi/server/ReportControllerTest.java
+++ eol-globi-rest/src/test/java/org/eol/globi/server/ReportControllerTest.java
@@ -21,7 +21,15 @@ public class ReportControllerTest {
                 "RETURN report.citation as study_citation, " +
                 "report.externalId as study_url, " +
                 "report.doi as study_doi, " +
-                "report.source as study_source_citation, report.nInteractions as number_of_interactions, report.nTaxa as number_of_distinct_taxa, report.nStudies as number_of_studies, report.nSources as number_of_sources, report.nTaxaNoMatch as number_of_distinct_taxa_no_match, report.sourceId as study_source_id SKIP 0 LIMIT 1024"));
+                "report.source as study_source_citation, " +
+                "report.nInteractions as number_of_interactions, " +
+                "report.nTaxa as number_of_distinct_taxa, " +
+                "report.nStudies as number_of_studies, " +
+                "report.nSources as number_of_sources, " +
+                "report.nTaxaNoMatch as number_of_distinct_taxa_no_match, " +
+                "report.sourceId as study_source_id " +
+                "SKIP 0 " +
+                "LIMIT 1024"));
         assertThat(source.getParams().get("sourceId"), is("globi:someSourceId"));
         CypherTestUtil.validate(source);
     }
@@ -31,7 +39,18 @@ public class ReportControllerTest {
         CypherQuery source = new ReportController().sources("bla:someSourceId", null);
         assertThat(source.getVersionedQuery(), is(CYPHER_VERSION +
                 "START report = node:reports(sourceId={sourceId}) " +
-                "RETURN report.citation as study_citation, report.externalId as study_url, report.doi as study_doi, report.source as study_source_citation, report.nInteractions as number_of_interactions, report.nTaxa as number_of_distinct_taxa, report.nStudies as number_of_studies, report.nSources as number_of_sources, report.nTaxaNoMatch as number_of_distinct_taxa_no_match, report.sourceId as study_source_id SKIP 0 LIMIT 1024"));
+                "RETURN report.citation as study_citation, " +
+                "report.externalId as study_url, " +
+                "report.doi as study_doi, " +
+                "report.source as study_source_citation, " +
+                "report.nInteractions as number_of_interactions, " +
+                "report.nTaxa as number_of_distinct_taxa, " +
+                "report.nStudies as number_of_studies, " +
+                "report.nSources as number_of_sources, " +
+                "report.nTaxaNoMatch as number_of_distinct_taxa_no_match, " +
+                "report.sourceId as study_source_id " +
+                "SKIP 0 " +
+                "LIMIT 1024"));
         assertThat(source.getParams().get("sourceId"), is("bla:someSourceId"));
         CypherTestUtil.validate(source);
     }
@@ -44,7 +63,22 @@ public class ReportControllerTest {
                 "dataset = node:datasets(namespace={namespace}), " +
                 "report = node:reports('sourceId:*') " +
                 "WHERE ('globi:' + dataset.namespace) = report.sourceId " +
-                "RETURN report.citation as study_citation, report.externalId as study_url, report.doi as study_doi, dataset.citation as study_source_citation, report.nInteractions as number_of_interactions, report.nTaxa as number_of_distinct_taxa, report.nStudies as number_of_studies, report.nSources as number_of_sources, report.nTaxaNoMatch as number_of_distinct_taxa_no_match, report.sourceId as study_source_id, dataset.doi as study_source_doi, dataset.format as study_source_format, dataset.archiveURI as study_source_archive_uri, dataset.lastSeenAt as study_source_last_seen_at SKIP 0 LIMIT 1024"));
+                "RETURN report.citation as study_citation, " +
+                "report.externalId as study_url, " +
+                "report.doi as study_doi, " +
+                "dataset.citation as study_source_citation, " +
+                "report.nInteractions as number_of_interactions, " +
+                "report.nTaxa as number_of_distinct_taxa, " +
+                "report.nStudies as number_of_studies, " +
+                "report.nSources as number_of_sources, " +
+                "report.nTaxaNoMatch as number_of_distinct_taxa_no_match, " +
+                "report.sourceId as study_source_id, " +
+                "dataset.doi as study_source_doi, " +
+                "dataset.format as study_source_format, " +
+                "dataset.archiveURI as study_source_archive_uri, " +
+                "dataset.lastSeenAt as study_source_last_seen_at " +
+                "SKIP 0 " +
+                "LIMIT 1024"));
         assertThat(source.getParams().get("namespace"), is("some/name"));
         CypherTestUtil.validate(source);
 
@@ -79,7 +113,22 @@ public class ReportControllerTest {
         assertThat(source.getVersionedQuery(), is(CYPHER_VERSION +
                 "START dataset = node:datasets(namespace={namespace}), report = node:reports('sourceId:*') " +
                 "WHERE ('globi:' + dataset.namespace) = report.sourceId " +
-                "RETURN report.citation as study_citation, report.externalId as study_url, report.doi as study_doi, dataset.citation as study_source_citation, report.nInteractions as number_of_interactions, report.nTaxa as number_of_distinct_taxa, report.nStudies as number_of_studies, report.nSources as number_of_sources, report.nTaxaNoMatch as number_of_distinct_taxa_no_match, report.sourceId as study_source_id, dataset.doi as study_source_doi, dataset.format as study_source_format, dataset.archiveURI as study_source_archive_uri, dataset.lastSeenAt as study_source_last_seen_at SKIP 0 LIMIT 1024"));
+                "RETURN report.citation as study_citation, " +
+                "report.externalId as study_url, " +
+                "report.doi as study_doi, " +
+                "dataset.citation as study_source_citation, " +
+                "report.nInteractions as number_of_interactions, " +
+                "report.nTaxa as number_of_distinct_taxa, " +
+                "report.nStudies as number_of_studies, " +
+                "report.nSources as number_of_sources, " +
+                "report.nTaxaNoMatch as number_of_distinct_taxa_no_match, " +
+                "report.sourceId as study_source_id, " +
+                "dataset.doi as study_source_doi, " +
+                "dataset.format as study_source_format, " +
+                "dataset.archiveURI as study_source_archive_uri, " +
+                "dataset.lastSeenAt as study_source_last_seen_at " +
+                "SKIP 0 " +
+                "LIMIT 1024"));
         assertThat(source.getParams().get("namespace"), is("some"));
         CypherTestUtil.validate(source);
 
@@ -91,7 +140,22 @@ public class ReportControllerTest {
         assertThat(source.getVersionedQuery(), is(CYPHER_VERSION +
                 "START dataset = node:datasets('namespace:*'), report = node:reports('sourceId:*') " +
                 "WHERE ('globi:' + dataset.namespace) = report.sourceId " +
-                "RETURN report.citation as study_citation, report.externalId as study_url, report.doi as study_doi, dataset.citation as study_source_citation, report.nInteractions as number_of_interactions, report.nTaxa as number_of_distinct_taxa, report.nStudies as number_of_studies, report.nSources as number_of_sources, report.nTaxaNoMatch as number_of_distinct_taxa_no_match, report.sourceId as study_source_id, dataset.doi as study_source_doi, dataset.format as study_source_format, dataset.archiveURI as study_source_archive_uri, dataset.lastSeenAt as study_source_last_seen_at SKIP 0 LIMIT 1024"));
+                "RETURN report.citation as study_citation, " +
+                "report.externalId as study_url, " +
+                "report.doi as study_doi, " +
+                "dataset.citation as study_source_citation, " +
+                "report.nInteractions as number_of_interactions, " +
+                "report.nTaxa as number_of_distinct_taxa, " +
+                "report.nStudies as number_of_studies, " +
+                "report.nSources as number_of_sources, " +
+                "report.nTaxaNoMatch as number_of_distinct_taxa_no_match, " +
+                "report.sourceId as study_source_id, " +
+                "dataset.doi as study_source_doi, " +
+                "dataset.format as study_source_format, " +
+                "dataset.archiveURI as study_source_archive_uri, " +
+                "dataset.lastSeenAt as study_source_last_seen_at " +
+                "SKIP 0 " +
+                "LIMIT 1024"));
         assertThat(source.getParams().size(), is(0));
         CypherTestUtil.validate(source);
 
@@ -102,7 +166,18 @@ public class ReportControllerTest {
         CypherQuery source = new ReportController().sources(null, null);
         assertThat(source.getVersionedQuery(), is(CYPHER_VERSION +
                 "START report = node:reports('sourceId:*') " +
-                "RETURN report.citation as study_citation, report.externalId as study_url, report.doi as study_doi, report.source as study_source_citation, report.nInteractions as number_of_interactions, report.nTaxa as number_of_distinct_taxa, report.nStudies as number_of_studies, report.nSources as number_of_sources, report.nTaxaNoMatch as number_of_distinct_taxa_no_match, report.sourceId as study_source_id SKIP 0 LIMIT 1024"));
+                "RETURN report.citation as study_citation, " +
+                "report.externalId as study_url, " +
+                "report.doi as study_doi, " +
+                "report.source as study_source_citation, " +
+                "report.nInteractions as number_of_interactions, " +
+                "report.nTaxa as number_of_distinct_taxa, " +
+                "report.nStudies as number_of_studies, " +
+                "report.nSources as number_of_sources, " +
+                "report.nTaxaNoMatch as number_of_distinct_taxa_no_match, " +
+                "report.sourceId as study_source_id " +
+                "SKIP 0 " +
+                "LIMIT 1024"));
         assertThat(source.getParams().size(), is(0));
         CypherTestUtil.validate(source);
     }
@@ -113,7 +188,17 @@ public class ReportControllerTest {
         assertThat(source.getVersionedQuery(), is(CYPHER_VERSION +
                 "START report = node:reports(source={source}) " +
                 "WHERE exists(report.title) " +
-                "RETURN report.citation as study_citation, report.externalId as study_url, report.doi as study_doi, report.source as study_source_citation, report.nInteractions as number_of_interactions, report.nTaxa as number_of_distinct_taxa, report.nStudies as number_of_studies, report.nSources as number_of_sources, report.nTaxaNoMatch as number_of_distinct_taxa_no_match SKIP 0 LIMIT 1024"));
+                "RETURN report.citation as study_citation, " +
+                "report.externalId as study_url, " +
+                "report.doi as study_doi, " +
+                "report.source as study_source_citation, " +
+                "report.nInteractions as number_of_interactions, " +
+                "report.nTaxa as number_of_distinct_taxa, " +
+                "report.nStudies as number_of_studies, " +
+                "report.nSources as number_of_sources, " +
+                "report.nTaxaNoMatch as number_of_distinct_taxa_no_match " +
+                "SKIP 0 " +
+                "LIMIT 1024"));
         assertThat(source.getParams().get("source"), is("a source"));
         CypherTestUtil.validate(source);
     }
@@ -124,7 +209,17 @@ public class ReportControllerTest {
         assertThat(source.getVersionedQuery(), is(CYPHER_VERSION +
                 "START report = node:reports('source:*') " +
                 "WHERE exists(report.title) " +
-                "RETURN report.citation as study_citation, report.externalId as study_url, report.doi as study_doi, report.source as study_source_citation, report.nInteractions as number_of_interactions, report.nTaxa as number_of_distinct_taxa, report.nStudies as number_of_studies, report.nSources as number_of_sources, report.nTaxaNoMatch as number_of_distinct_taxa_no_match SKIP 0 LIMIT 1024"));
+                "RETURN report.citation as study_citation, " +
+                "report.externalId as study_url, " +
+                "report.doi as study_doi, " +
+                "report.source as study_source_citation, " +
+                "report.nInteractions as number_of_interactions, " +
+                "report.nTaxa as number_of_distinct_taxa, " +
+                "report.nStudies as number_of_studies, " +
+                "report.nSources as number_of_sources, " +
+                "report.nTaxaNoMatch as number_of_distinct_taxa_no_match " +
+                "SKIP 0 " +
+                "LIMIT 1024"));
         assertThat(source.getParams().size(), is(0));
         CypherTestUtil.validate(source);
     }
