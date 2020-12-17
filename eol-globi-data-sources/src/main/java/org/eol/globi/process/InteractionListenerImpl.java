8c24422904ff3c97360761a31e1d463beffb0082
jhpoelen <jhpoelen@xs4all.nl>
2020-12-17 15:39:26 -0800

    interaction listener refactoring continued....

--- eol-globi-data-sources/src/main/java/org/eol/globi/process/InteractionListenerImpl.java
+++ eol-globi-data-sources/src/main/java/org/eol/globi/process/InteractionListenerImpl.java
@@ -1,32 +1,49 @@
 package org.eol.globi.process;
 
-import org.apache.commons.lang3.StringUtils;
 import org.eol.globi.data.ImportLogger;
 import org.eol.globi.data.NodeFactory;
 import org.eol.globi.data.StudyImporterException;
 import org.eol.globi.service.GeoNamesService;
-import org.joda.time.Interval;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 public class InteractionListenerImpl implements InteractionListener {
 
-    private final InteractionListener processor;
+    private final List<InteractionListener> processors;
+    private final InteractionListener queue;
+    private List<Map<String, String>> interactions = new ArrayList<>();
 
     public InteractionListenerImpl(NodeFactory nodeFactory, GeoNamesService geoNamesService, ImportLogger logger) {
-        this.processor =
-                new InteractionExpander(
-                        new InteractionValidator(
-                                new InteractionImporter(nodeFactory, logger, geoNamesService),
-                                logger
-                        ), logger
+        this.queue = interaction -> {
+            if (interaction != null) {
+                interactions.add(interaction);
+            }
+        };
+        this.processors =
+                Arrays.asList(
+                        new InteractionExpander(queue, logger),
+                        new InteractionValidator(queue, logger),
+                        new InteractionImporter(nodeFactory, logger, geoNamesService)
                 );
     }
 
     @Override
     public void on(Map<String, String> interaction) throws StudyImporterException {
+        queue.on(interaction);
         try {
-            processor.on(interaction);
+            for (InteractionListener processor : processors) {
+                if (interactions.isEmpty()) {
+                    break;
+                }
+                ArrayList<Map<String, String>> incoming = new ArrayList<>(interactions);
+                interactions.clear();
+                for (Map<String, String> incomingInteractions : incoming) {
+                    processor.on(incomingInteractions);
+                }
+            }
         } catch (StudyImporterException e) {
             throw new StudyImporterException("failed to import: " + interaction, e);
         }
