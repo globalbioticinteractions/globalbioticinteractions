9c048aa2001a29274887d70633baea41f44664dd
jhpoelen <jhpoelen@xs4all.nl>
2021-09-14 11:38:11 -0700

    add logging for batch processing

--- eol-globi-neo4j/src/main/java/org/eol/globi/util/NodeProcessorImpl.java
+++ eol-globi-neo4j/src/main/java/org/eol/globi/util/NodeProcessorImpl.java
@@ -1,11 +1,16 @@
 package org.eol.globi.util;
 
+import org.apache.commons.lang3.time.StopWatch;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class NodeProcessorImpl implements NodeProcessor<NodeListener> {
+
+    private final static Logger LOG = LoggerFactory.getLogger(NodeProcessorImpl.class);
 
     private final GraphDatabaseService graphService;
     private final Long batchSize;
@@ -49,17 +54,25 @@ public class NodeProcessorImpl implements NodeProcessor<NodeListener> {
         final AtomicLong nodeCount = new AtomicLong(0L);
         do {
             batchListener.onStart();
+            StopWatch stopWatch = new StopWatch();
+            stopWatch.start();
+            LOG.info("requesting batch of [" + batchSize + "] [" + indexName + "] nodes");
             nodeIds = NodeUtil.getBatchOfNodes(graphService,
                     nodeCount.get(),
                     batchSize,
                     queryKey,
                     queryOrQueryObject,
                     indexName);
+            stopWatch.split();
+            LOG.info("received batch of [" + batchSize + "] [" + indexName + "] nodes in [" + stopWatch.getTime() +"] ms");
             for (Long nodeId : nodeIds) {
                 nodeListener.on(graphService.getNodeById(nodeId));
                 nodeCount.incrementAndGet();
             }
             batchListener.onFinish();
+            stopWatch.stop();
+            LOG.info("processed batch of [" + batchSize + "] [" + indexName + "] nodes in [" + stopWatch.getTime() +"] ms.");
+            LOG.info("total processed [" + indexName + "] nodes so far:  [" + nodeCount.get() +"]");
         } while (!nodeIds.isEmpty());
     }
 }
