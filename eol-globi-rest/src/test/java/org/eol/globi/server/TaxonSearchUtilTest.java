0bcf96b7a50cf9094ee0590122529ed8f1fa69db
jhpoelen <jhpoelen@xs4all.nl>
2021-09-16 17:51:43 -0700

    attempt to fix test errors

--- eol-globi-rest/src/test/java/org/eol/globi/server/TaxonSearchUtilTest.java
+++ eol-globi-rest/src/test/java/org/eol/globi/server/TaxonSearchUtilTest.java
@@ -13,7 +13,7 @@ public class TaxonSearchUtilTest {
     public void createQuery() {
         CypherQuery query = TaxonSearchUtil.getCypherQuery("Animalia", new HashMap());
         assertThat(query.getVersionedQuery(), Is.is("CYPHER 2.3 START someTaxon = node:taxons({pathQuery}) MATCH someTaxon-[:SAME_AS*0..1]->taxon WHERE exists(taxon.externalId) WITH DISTINCT(taxon.externalId) as externalId, taxon.externalUrl as externalUrl RETURN externalId as taxon_external_id,externalUrl as taxon_external_url"));
-        assertThat(query.getParams().toString(), Is.is("{pathQuery=name:\\\"Animalia\\\"}"));
+        assertThat(query.getParams().toString(), Is.is("{pathQuery=name:\"Animalia\"}"));
         CypherTestUtil.validate(query);
     }
 
@@ -21,7 +21,7 @@ public class TaxonSearchUtilTest {
     public void createQueryWithSupportedId() {
         CypherQuery query = TaxonSearchUtil.getCypherQuery("EOL:123", new HashMap());
         assertThat(query.getVersionedQuery(), Is.is("CYPHER 2.3 START someTaxon = node:taxons({pathQuery}) MATCH someTaxon-[:SAME_AS*0..1]->taxon WHERE exists(taxon.externalId) WITH DISTINCT(taxon.externalId) as externalId, taxon.externalUrl as externalUrl RETURN externalId as taxon_external_id,externalUrl as taxon_external_url"));
-        assertThat(query.getParams().toString(), Is.is("{pathQuery=externalId:\\\"EOL:123\\\"}"));
+        assertThat(query.getParams().toString(), Is.is("{pathQuery=externalId:\"EOL:123\"}"));
         CypherTestUtil.validate(query);
     }
 
@@ -29,7 +29,7 @@ public class TaxonSearchUtilTest {
     public void createQueryWithSupportedId2() {
         CypherQuery query = TaxonSearchUtil.getCypherQuery("http://taxon-concept.plazi.org/id/Animalia/Caridae_Dana_1852", new HashMap());
         assertThat(query.getVersionedQuery(), Is.is("CYPHER 2.3 START someTaxon = node:taxons({pathQuery}) MATCH someTaxon-[:SAME_AS*0..1]->taxon WHERE exists(taxon.externalId) WITH DISTINCT(taxon.externalId) as externalId, taxon.externalUrl as externalUrl RETURN externalId as taxon_external_id,externalUrl as taxon_external_url"));
-        assertThat(query.getParams().toString(), Is.is("{pathQuery=externalId:\\\"http://taxon-concept.plazi.org/id/Animalia/Caridae_Dana_1852\\\"}"));
+        assertThat(query.getParams().toString(), Is.is("{pathQuery=externalId:\"http://taxon-concept.plazi.org/id/Animalia/Caridae_Dana_1852\"}"));
         CypherTestUtil.validate(query);
     }
 
@@ -37,7 +37,7 @@ public class TaxonSearchUtilTest {
     public void createQueryWithUnsupportedId() {
         CypherQuery query = TaxonSearchUtil.getCypherQuery("FOO:1", new HashMap());
         assertThat(query.getVersionedQuery(), Is.is("CYPHER 2.3 START someTaxon = node:taxons({pathQuery}) MATCH someTaxon-[:SAME_AS*0..1]->taxon WHERE exists(taxon.externalId) WITH DISTINCT(taxon.externalId) as externalId, taxon.externalUrl as externalUrl RETURN externalId as taxon_external_id,externalUrl as taxon_external_url"));
-        assertThat(query.getParams().toString(), Is.is("{pathQuery=name:\\\"FOO:1\\\"}"));
+        assertThat(query.getParams().toString(), Is.is("{pathQuery=name:\"FOO:1\"}"));
         CypherTestUtil.validate(query);
     }
 
