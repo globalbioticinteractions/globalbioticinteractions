b612f83eac0ab6df23ac08d57c91c4b6787acdc2
jhpoelen <jhpoelen@xs4all.nl>
2022-04-29 08:15:53 -0500

    remove opentree module after migration to nomer https://github.com/globalbioticinteractions/nomer/issues/30

--- eol-globi-opentree/src/main/java/org/eol/globi/opentree/OpenTreeListener.java
+++
@@ -1,5 +0,0 @@
-package org.eol.globi.opentree;
-
-interface OpenTreeListener {
-    void taxonSameAs(String ottId, String nonOttId);
-}
--- eol-globi-opentree/src/main/java/org/eol/globi/opentree/TaxonListener.java
+++
@@ -1,5 +0,0 @@
-package org.eol.globi.opentree;
-
-interface TaxonListener {
-    void addTaxonId(String taxonId);
-}
