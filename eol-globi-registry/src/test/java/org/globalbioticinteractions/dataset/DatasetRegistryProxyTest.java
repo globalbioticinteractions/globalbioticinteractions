240455ccb8555ba291138206940c6ce5f0ef9341
jhpoelen <jhpoelen@xs4all.nl>
2022-09-13 15:51:20 -0500

    upgrade to springframework v5.2.22; related to https://github.com/globalbioticinteractions/nomer/issues/44

--- eol-globi-registry/src/test/java/org/globalbioticinteractions/dataset/DatasetRegistryProxyTest.java
+++ eol-globi-registry/src/test/java/org/globalbioticinteractions/dataset/DatasetRegistryProxyTest.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.CoreMatchers.hasItem;
 
 public class DatasetRegistryProxyTest {
 
