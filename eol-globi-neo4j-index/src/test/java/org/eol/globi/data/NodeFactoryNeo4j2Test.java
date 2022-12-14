8530a0aa69ab53420653a278cdfe50e483a08a9d
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2022-12-14 14:51:51 -0600

    reproduce https://github.com/globalbioticinteractions/globalbioticinteractions/issues/857

--- eol-globi-neo4j-index/src/test/java/org/eol/globi/data/NodeFactoryNeo4j2Test.java
+++ eol-globi-neo4j-index/src/test/java/org/eol/globi/data/NodeFactoryNeo4j2Test.java
@@ -1,6 +1,16 @@
 package org.eol.globi.data;
 
+import org.eol.globi.domain.LocationImpl;
+import org.eol.globi.domain.LocationNode;
+import org.hamcrest.core.Is;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.nullValue;
+
 public class NodeFactoryNeo4j2Test extends NodeFactoryNeo4jTest {
+
+    public static final int MAX_INDEX_LENGTH_TIMES_TWO = ((1 << 15) - 2) * 2;
 
     @Override
     protected NodeFactoryNeo4j createNodeFactory() {
@@ -11,4 +21,29 @@ public class NodeFactoryNeo4j2Test extends NodeFactoryNeo4jTest {
     }
 
 
+    @Test(expected = IllegalArgumentException.class)
+    public void indexLongLocationValueString() throws NodeFactoryException {
+        LocationImpl location2 = new LocationImpl(1.2d, 1.4d, -1.0d, null);
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < MAX_INDEX_LENGTH_TIMES_TWO; i++) {
+            builder.append("a");
+        }
+        location2.setLocality(builder.toString());
+        location2.setLocalityId("some:id");
+        LocationNode orCreateLocation = getNodeFactory().getOrCreateLocation(location2);
+        assertThat(orCreateLocation, Is.is(nullValue()));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void indexLongLocationIdString() throws NodeFactoryException {
+        LocationImpl location2 = new LocationImpl(1.2d, 1.4d, -1.0d, null);
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < MAX_INDEX_LENGTH_TIMES_TWO; i++) {
+            builder.append("a");
+        }
+        location2.setLocality("some:value");
+        location2.setLocalityId(builder.toString());
+        LocationNode orCreateLocation = getNodeFactory().getOrCreateLocation(location2);
+        assertThat(orCreateLocation, Is.is(nullValue()));
+    }
 }
