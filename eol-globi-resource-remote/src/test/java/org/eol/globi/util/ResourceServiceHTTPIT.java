f215052602e47c78dd0970bb8e4733560f386ca5
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2023-09-29 06:27:54 -0500

    reproduce error in https://github.com/globalbioticinteractions/globalbioticinteractions/issues/923#issuecomment-1740698215; fyi @tkuhn

---
+++ eol-globi-resource-remote/src/test/java/org/eol/globi/util/ResourceServiceHTTPIT.java
@@ -0,0 +1,17 @@
+package org.eol.globi.util;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class ResourceServiceHTTPIT {
+
+
+    @Test
+    public void retrieve() throws IOException {
+        ResourceServiceHTTP service = new ResourceServiceHTTP(is -> is);
+        service.retrieve(URI.create("https://grlc.knowledgepixels.com/api-git/knowledgepixels/bdj-nanopub-api/get-taxontaxon-nanopubs.csv"));
+    }
+
+}
