8897c4ac15f3da5f6d3498ca4f49d9bc6d0c4b91
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2024-07-01 14:30:01 -0500

    remove unused import

--- eol-globi-lib/src/main/java/org/eol/globi/data/FileUtils.java
+++ eol-globi-lib/src/main/java/org/eol/globi/data/FileUtils.java
@@ -9,7 +9,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.zip.GZIPInputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
