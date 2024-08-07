5cf555905c3e2de1c5dbc7b0520cf9ef4ce09fcb
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2024-08-07 11:02:46 -0500

    fix test error

--- eol-globi-cache/src/test/java/org/globalbioticinteractions/cache/CachePullThroughTest.java
+++ eol-globi-cache/src/test/java/org/globalbioticinteractions/cache/CachePullThroughTest.java
@@ -4,7 +4,9 @@ import org.eol.globi.util.InputStreamFactoryNoop;
 import org.eol.globi.util.ResourceServiceLocal;
 import org.eol.globi.util.ResourceServiceLocalAndRemote;
 import org.hamcrest.CoreMatchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,12 +14,22 @@ import java.net.URI;
 
 import static org.hamcrest.core.StringStartsWith.startsWith;
 import static org.hamcrest.MatcherAssert.assertThat;
+
 public class CachePullThroughTest {
+
+    @Rule
+
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
     public void cache() throws IOException {
         File cacheDir = CacheUtil.findOrMakeCacheDirForNamespace("target/cache/datasets", "some/namespace");
-        ContentProvenance contentProvenance = CachePullThrough.cache(URI.create("https://github.com/globalbioticinteractions/template-dataset/archive/main.zip"), cacheDir, new ResourceServiceLocal(new InputStreamFactoryNoop()));
+        ContentProvenance contentProvenance
+                = CachePullThrough.cache(
+                URI.create("https://github.com/globalbioticinteractions/template-dataset/archive/main.zip"),
+                cacheDir,
+                new ResourceServiceLocalAndRemote(new InputStreamFactoryNoop(), folder.newFolder())
+        );
         File cachedFile = new File(contentProvenance.getLocalURI());
         assertThat(cachedFile.exists(), CoreMatchers.is(true));
         assertThat(cachedFile.toURI().toString(), startsWith("file:/"));
