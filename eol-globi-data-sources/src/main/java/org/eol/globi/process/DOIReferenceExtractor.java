a2f09b3be62a5bfbfe9a47d2160d76d495110a16
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-07-22 21:38:50 -0500

    extract doi urls (e.g., "https://doi.org/10.12.345") in addition to print doi patterns (e.g., "doi:10.12/345")

--- eol-globi-data-sources/src/main/java/org/eol/globi/process/DOIReferenceExtractor.java
+++ eol-globi-data-sources/src/main/java/org/eol/globi/process/DOIReferenceExtractor.java
@@ -1,19 +1,23 @@
 package org.eol.globi.process;
 
 import org.apache.commons.lang3.StringUtils;
-import org.eol.globi.data.AssociatedTaxaUtil;
 import org.eol.globi.data.DatasetImporterForTSV;
 import org.eol.globi.data.ImportLogger;
 import org.eol.globi.data.StudyImporterException;
 import org.globalbioticinteractions.doi.DOI;
+import org.globalbioticinteractions.doi.MalformedDOIException;
 
-import java.util.List;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DOIReferenceExtractor extends InteractionProcessorAbstract {
+
+    private static final Pattern PATTERN_DOI = Pattern.compile(".*(doi:[ ]*)(10[.])(?<doiRegistrant>.*)/(?<doiSuffix>[^ ]+)");
+    private static final Pattern PATTERN_DOI_URI = Pattern.compile(".*(?<doiUrl>http[s]{0,1}://(dx[.]){0,1}(doi.org/)(10[.])(?<doiRegistrant>.*)/(?<doiSuffix>[^ ]+))");
 
     public DOIReferenceExtractor(InteractionListener listener, ImportLogger logger) {
         super(listener, logger);
@@ -29,22 +33,61 @@ public class DOIReferenceExtractor extends InteractionProcessorAbstract {
 
             String referenceCitation = interaction.get(DatasetImporterForTSV.REFERENCE_CITATION);
             if (StringUtils.isNoneBlank(referenceCitation)) {
-                Matcher matcher = Pattern.compile(".*(doi:)([ ]*)(10[.])(.*)/([^ ]+)").matcher(referenceCitation);
-                if (matcher.matches()) {
-                    String registrant = matcher.group(4);
-                    String suffix = matcher.group(5);
-                    try {
-                        DOI doi = new DOI(registrant, suffix);
-                        emittingInteraction = new TreeMap<String, String>(interaction) {{
-                            put(DatasetImporterForTSV.REFERENCE_DOI, doi.toString());
-                        }};
-                    } catch (IllegalArgumentException ex) {
-                        // ignore
-                    }
+
+                DOI doi = extractDoi(referenceCitation);
+
+                if (doi == null) {
+                    doi = extractDoiByURLPattern(referenceCitation);
                 }
+
+                if (doi != null) {
+                    emittingInteraction = new TreeMap<>(interaction);
+                    emittingInteraction.put(DatasetImporterForTSV.REFERENCE_DOI, doi.toString());
+                }
+
             }
         }
         emit(emittingInteraction);
+    }
+
+    private static DOI extractDoi(String referenceCitation) {
+        DOI doi = null;
+        Matcher matcher = PATTERN_DOI.matcher(referenceCitation);
+        if (matcher.matches()) {
+            String registrant = matcher.group("doiRegistrant");
+            String suffix = StringUtils.trim(matcher.group("doiSuffix"));
+            suffix = stripTrailingPeriod(suffix);
+
+            try {
+                doi = new DOI(registrant, suffix);
+            } catch (IllegalArgumentException ex) {
+                // ignore
+            }
+        }
+        return doi;
+    }
+
+    private static DOI extractDoiByURLPattern(String textToMatch) {
+        DOI doi = null;
+        Matcher urlMatcher = PATTERN_DOI_URI.matcher(textToMatch);
+        if (urlMatcher.matches()) {
+            try {
+                String doiUrl1 = urlMatcher.group("doiUrl");
+                doiUrl1 = stripTrailingPeriod(doiUrl1);
+                URI doiUrl = new URI(doiUrl1);
+                doi = DOI.create(doiUrl);
+            } catch (URISyntaxException | MalformedDOIException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return doi;
+    }
+
+    private static String stripTrailingPeriod(String doiUrl1) {
+        doiUrl1 = StringUtils.endsWith(doiUrl1, ".")
+                ? StringUtils.substring(doiUrl1, 0, doiUrl1.length()-1)
+                : doiUrl1;
+        return doiUrl1;
     }
 
 }
--- eol-globi-data-sources/src/test/java/org/eol/globi/process/DOIReferenceExtractorTest.java
+++ eol-globi-data-sources/src/test/java/org/eol/globi/process/DOIReferenceExtractorTest.java
@@ -16,6 +16,47 @@ import static org.hamcrest.Matchers.nullValue;
 public class DOIReferenceExtractorTest {
 
     @Test
+    public void extractDOIFromCitationWithDOIUrl() throws StudyImporterException {
+        List<Map<String, String>> interactions = new ArrayList<>();
+
+        DOIReferenceExtractor doiReferenceExtractor = new DOIReferenceExtractor(new InteractionListener() {
+            @Override
+            public void on(Map<String, String> interaction) throws StudyImporterException {
+                interactions.add(interaction);
+            }
+        }, null);
+
+        doiReferenceExtractor.on(new TreeMap<String, String>() {{
+            put(DatasetImporterForTSV.REFERENCE_CITATION, "Ollerton, J., Trunschke, J. ., Havens, K. ., Landaverde-González, P. ., Keller, A. ., Gilpin, A.-M. ., Rodrigo Rech, A. ., Baronio, G. J. ., Phillips, B. J., Mackin, C. ., Stanley, D. A., Treanore, E. ., Baker, E. ., Rotheray, E. L., Erickson, E. ., Fornoff, F. ., Brearley, F. Q. ., Ballantyne, G. ., Iossa, G. ., Stone, G. N., Bartomeus, I. ., Stockan, J. A., Leguizamón, J., Prendergast, K. ., Rowley, L., Giovanetti, M., de Oliveira Bueno, R., Wesselingh, R. A., Mallinger, R., Edmondson, S., Howard, S. R., Leonhardt, S. D., Rojas-Nossa, S. V., Brett, M., Joaqui, T., Antoniazzi, R., Burton, V. J., Feng, H.-H., Tian, Z.-X., Xu, Q., Zhang, C., Shi, C.-L., Huang, S.-Q., Cole, L. J., Bendifallah, L., Ellis, E. E., Hegland, S. J., Straffon Díaz, S., Lander, T. A. ., Mayr, A. V., Dawson, R. ., Eeraerts, M. ., Armbruster, W. S. ., Walton, B. ., Adjlane, N. ., Falk, S. ., Mata, L. ., Goncalves Geiger, A. ., Carvell, C. ., Wallace, C. ., Ratto, F. ., Barberis, M. ., Kahane, F. ., Connop, S. ., Stip, A. ., Sigrist, M. R. ., Vereecken, N. J. ., Klein, A.-M., Baldock, K. ., & Arnold, S. E. J. . (2022). Pollinator-flower interactions in gardens during the COVID-19 pandemic lockdown of 2020. Journal of Pollination Ecology, 31, 87–96. https://doi.org/10.26786/1920-7603(2022)695.");
+        }});
+
+        assertThat(interactions.size(), is(1));
+
+        assertThat(interactions.get(0).get(DatasetImporterForTSV.REFERENCE_DOI), is("10.26786/1920-7603(2022)695"));
+
+    }
+    @Test
+    public void extractDOIFromCitationWithDoi() throws StudyImporterException {
+        List<Map<String, String>> interactions = new ArrayList<>();
+
+        DOIReferenceExtractor doiReferenceExtractor = new DOIReferenceExtractor(new InteractionListener() {
+            @Override
+            public void on(Map<String, String> interaction) throws StudyImporterException {
+                interactions.add(interaction);
+            }
+        }, null);
+
+        doiReferenceExtractor.on(new TreeMap<String, String>() {{
+            put(DatasetImporterForTSV.REFERENCE_CITATION,
+                    "Ollerton, J., Trunschke, J. ., Havens, K. ., Landaverde-González, P. ., Keller, A. ., Gilpin, A.-M. ., Rodrigo Rech, A. ., Baronio, G. J. ., Phillips, B. J., Mackin, C. ., Stanley, D. A., Treanore, E. ., Baker, E. ., Rotheray, E. L., Erickson, E. ., Fornoff, F. ., Brearley, F. Q. ., Ballantyne, G. ., Iossa, G. ., Stone, G. N., Bartomeus, I. ., Stockan, J. A., Leguizamón, J., Prendergast, K. ., Rowley, L., Giovanetti, M., de Oliveira Bueno, R., Wesselingh, R. A., Mallinger, R., Edmondson, S., Howard, S. R., Leonhardt, S. D., Rojas-Nossa, S. V., Brett, M., Joaqui, T., Antoniazzi, R., Burton, V. J., Feng, H.-H., Tian, Z.-X., Xu, Q., Zhang, C., Shi, C.-L., Huang, S.-Q., Cole, L. J., Bendifallah, L., Ellis, E. E., Hegland, S. J., Straffon Díaz, S., Lander, T. A. ., Mayr, A. V., Dawson, R. ., Eeraerts, M. ., Armbruster, W. S. ., Walton, B. ., Adjlane, N. ., Falk, S. ., Mata, L. ., Goncalves Geiger, A. ., Carvell, C. ., Wallace, C. ., Ratto, F. ., Barberis, M. ., Kahane, F. ., Connop, S. ., Stip, A. ., Sigrist, M. R. ., Vereecken, N. J. ., Klein, A.-M., Baldock, K. ., & Arnold, S. E. J. . (2022). Pollinator-flower interactions in gardens during the COVID-19 pandemic lockdown of 2020. Journal of Pollination Ecology, 31, 87–96. doi:10.26786/1920-7603(2022)695.");
+        }});
+
+        assertThat(interactions.size(), is(1));
+
+        assertThat(interactions.get(0).get(DatasetImporterForTSV.REFERENCE_DOI), is("10.26786/1920-7603(2022)695"));
+
+    }
+    @Test
     public void extractDOI() throws StudyImporterException {
         List<Map<String, String>> interactions = new ArrayList<>();
 
