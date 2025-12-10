0993a39b0c9cc641052f5fd1407d6b9d5dc23ff6
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-12-10 17:56:39 -0600

    move wikidata network-dependent integration test to IT

--- wikidata/src/test/java/org/globalbioticinteractions/wikidata/WikidataUtilIT.java
+++ wikidata/src/test/java/org/globalbioticinteractions/wikidata/WikidataUtilIT.java
@@ -1,11 +1,14 @@
 package org.globalbioticinteractions.wikidata;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.eol.globi.domain.Taxon;
 import org.eol.globi.domain.TaxonomyProvider;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -34,6 +37,139 @@ public class WikidataUtilIT {
         }
         assertThat("no mapping for wikidata taxon providers [" + unsupported.stream().collect(Collectors.joining(" ")) + "]"
                 , unsupported, is(empty()));
+    }
+
+    @Test
+    public void lookupTaxonLinks() throws IOException, URISyntaxException {
+        List<Taxon> relatedTaxonIds =
+                WikidataUtil.findRelatedTaxonIds("NCBI:9606");
+
+        final String ids = relatedTaxonIds
+                .stream()
+                .map(Taxon::getExternalId)
+                .sorted()
+                .collect(Collectors.joining("|"));
+
+        assertThat(ids, is("BOLDTaxon:12439|COL:6MB3T|EOL:327955|GBIF:2436436|INAT_TAXON:43584|IRMNG:10857762|ITIS:180092|MSW:12100795|NBN:NHMSYS0000376773|NCBI:9606|OTT:770315|WD:Q15978631|WORMS:1455977"));
+
+        final String names = relatedTaxonIds
+                .stream()
+                .map(Taxon::getName)
+                .distinct()
+                .collect(Collectors.joining("|"));
+
+        assertThat(names, is("Homo sapiens"));
+    }
+
+    @Test
+    public void lookupTaxonLinksPlant() throws IOException, URISyntaxException {
+        List<Taxon> relatedTaxonIds =
+                WikidataUtil.findRelatedTaxonIds("WD:Q332469");
+
+        final String ids = relatedTaxonIds
+                .stream()
+                .map(Taxon::getExternalId)
+                .sorted()
+                .collect(Collectors.joining("|"));
+
+        assertThat(ids, is("COL:99P2F|EOL:579775|GBIF:2925303|INAT_TAXON:50333|IRMNG:10206228|ITIS:32175|NCBI:126435|OTT:78889|WD:Q332469|WFO:wfo-0000223016"));
+
+        final String names = relatedTaxonIds
+                .stream()
+                .map(Taxon::getName)
+                .distinct()
+                .collect(Collectors.joining("|"));
+
+        assertThat(names, is("Lantana camara"));
+    }
+
+    @Test
+    public void lookupTaxonLinksIndexFungorum() throws IOException, URISyntaxException {
+        List<Taxon> relatedTaxonIds =
+                WikidataUtil.findRelatedTaxonIds("IF:7106");
+
+        final String ids = relatedTaxonIds
+                .stream()
+                .map(Taxon::getExternalId)
+                .sorted()
+                .collect(Collectors.joining("|"));
+
+        assertThat(ids, is("COL:SYV|EOL:16498|GBIF:2616104|IF:7106|INAT_TAXON:327996|IRMNG:1312559|ITIS:14134|NBN:NHMSYS0001474393|NCBI:5598|OTT:464790|WD:Q133266|WORMS:100208"));
+
+        final String names = relatedTaxonIds
+                .stream()
+                .map(Taxon::getName)
+                .distinct()
+                .collect(Collectors.joining("|"));
+
+        assertThat(names, is("Alternaria"));
+    }
+
+    @Test
+    public void lookupTaxonLinksByWDEntry() throws IOException, URISyntaxException {
+        List<Taxon> relatedTaxonIds =
+                WikidataUtil.findRelatedTaxonIds("WD:Q15978631");
+
+        final String ids = relatedTaxonIds
+                .stream()
+                .map(Taxon::getExternalId)
+                .sorted()
+                .collect(Collectors.joining("|"));
+
+        assertThat(ids, is("BOLDTaxon:12439|COL:6MB3T|EOL:327955|GBIF:2436436|INAT_TAXON:43584|IRMNG:10857762|ITIS:180092|MSW:12100795|NBN:NHMSYS0000376773|NCBI:9606|OTT:770315|WD:Q15978631|WORMS:1455977"));
+
+        final String names = relatedTaxonIds
+                .stream()
+                .map(Taxon::getName)
+                .distinct()
+                .collect(Collectors.joining("|"));
+
+        assertThat(names, is("Homo sapiens"));
+    }
+
+    @Test
+    public void createITISLionQuery() throws IOException {
+        String sparqlQuery = WikidataUtil.createSparqlQuery("ITIS:183803", "en");
+
+        String expectedQuery = IOUtils.toString(getClass().getResourceAsStream("itis.sparql"), StandardCharsets.UTF_8);
+
+        assertThat(sparqlQuery, is(expectedQuery));
+    }
+
+    @Test
+    public void createPlaziRhinolophusDentiQuery() throws IOException {
+        String sparqlQuery = WikidataUtil.createSparqlQuery("PLAZI:885887A2FFC88A21F8B1FA48FB92DD65", "en");
+
+        String expected = IOUtils.toString(getClass().getResourceAsStream("plazi.sparql"), StandardCharsets.UTF_8);
+
+        assertThat(sparqlQuery, is(expected));
+    }
+
+    @Test
+    public void createPlaziAnguillaAnguillaFrenchQuery() throws IOException {
+        String sparqlQuery = WikidataUtil.createSparqlQuery("PLAZI:94AA5349-EDA9-E45C-E646-5F479A5A4F1A", "fr");
+
+        String expected = IOUtils.toString(getClass().getResourceAsStream("plazi_french.sparql"), StandardCharsets.UTF_8);
+
+        assertThat(sparqlQuery, is(expected));
+    }
+
+    @Test
+    public void createWikiDataLionEnglishQuery() throws IOException {
+        String sparqlQuery = WikidataUtil.createSparqlQuery("WD:Q140", "en");
+
+        String expected = IOUtils.toString(getClass().getResourceAsStream("wikidata_english.sparql"), StandardCharsets.UTF_8);
+
+        assertThat(sparqlQuery, is(expected));
+    }
+
+    @Test
+    public void createWikiDataLionPortugueseQuery() throws IOException {
+        String sparqlQuery = WikidataUtil.createSparqlQuery("WD:Q140", "pt");
+
+        String expected = IOUtils.toString(getClass().getResourceAsStream("wikidata_portuguese.sparql"), StandardCharsets.UTF_8);
+
+        assertThat(sparqlQuery, is(expected));
     }
 
 }
