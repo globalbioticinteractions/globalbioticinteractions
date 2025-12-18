96ba4e85b54a4302852cddd93980af372992fd76
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-12-17 18:59:22 -0600

    remove unused travis config

--- .travis.deploy_if_tag.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -xe
-if [ -n "$TRAVIS_TAG" ]; then
-  mvn -s .travis.maven.settings.xml -DskipTests clean deploy
-fi
--- .travis.version_if_tag.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [ -n "$TRAVIS_TAG" ]; then
-  VERSION=${TRAVIS_TAG//[^0-9.]/}
-  mvn -pl eol-globi-parent versions:set -DnewVersion=$VERSION
-fi
