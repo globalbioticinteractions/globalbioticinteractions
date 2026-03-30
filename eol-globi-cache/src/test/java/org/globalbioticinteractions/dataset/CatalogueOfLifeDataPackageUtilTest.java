06640481909bd7f50edb6f47a602f270719d30b0
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2026-03-30 17:52:48 -0500

    attempt to align taxonworks/hobern catalogue of life data package dialects; https://github.com/globalbioticinteractions/globalbioticinteractions/issues/753

--- eol-globi-cache/src/test/java/org/globalbioticinteractions/dataset/CatalogueOfLifeDataPackageUtilTest.java
+++ eol-globi-cache/src/test/java/org/globalbioticinteractions/dataset/CatalogueOfLifeDataPackageUtilTest.java
@@ -16,6 +16,8 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.TreeMap;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertNotNull;
@@ -68,13 +70,20 @@ public class CatalogueOfLifeDataPackageUtilTest {
 
         assertNotNull(jsonNode);
 
+        Map<String, Integer> tableColumns = new TreeMap<>();
         JsonNode tablesNode = jsonNode.at("/tables");
         assertThat(tablesNode.size(), Is.is(4));
         for (JsonNode table : tablesNode) {
+            tableColumns.put(table.at("/url").asText(), table.at("/tableSchema/columns").size());
             assertThat(table.at("/dcterms:bibliographicCitation").asText(),
                     Is.is("@misc{ChecklistBankDataset2317, version = {Mar 2026}, url = {https://hoppers.speciesfile.org/}, title = {3i World Auchenorrhyncha Database}, author = {{Dmitriev}, {D.A.}}, year = 2026, month = 3}")
             );
         }
+
+        assertThat(tableColumns.get("Taxon.tsv"), Is.is(18));
+        assertThat(tableColumns.get("Name.tsv"), Is.is(21));
+        assertThat(tableColumns.get("References.tsv"), Is.is(5));
+        assertThat(tableColumns.get("SpeciesInteraction.tsv"), Is.is(8));
     }
 
     @Test
@@ -95,13 +104,21 @@ public class CatalogueOfLifeDataPackageUtilTest {
 
         assertNotNull(jsonNode);
 
+        Map<String, Integer> tableColumns = new TreeMap<>();
         JsonNode tablesNode = jsonNode.at("/tables");
         assertThat(tablesNode.size(), Is.is(4));
         for (JsonNode table : tablesNode) {
+            tableColumns.put(table.at("/url").asText(), table.at("/tableSchema/columns").size());
             assertThat(table.at("/dcterms:bibliographicCitation").asText(),
                     Is.is("@misc{ChecklistBankDataset2362, version = {1.1.26.046}, title = {Catalogue of World Gelechiidae}, author = {{Hobern}, {Donald} and {Sattler}, {Klaus}}, year = 2026, month = 2}")
             );
         }
+
+        assertThat(tableColumns.get("data/taxon.csv"), Is.is(22));
+        assertThat(tableColumns.get("data/name.csv"), Is.is(17));
+        assertThat(tableColumns.get("data/reference.csv"), Is.is(10));
+        assertThat(tableColumns.get("data/speciesinteraction.csv"), Is.is(11));
+
 
     }
 
