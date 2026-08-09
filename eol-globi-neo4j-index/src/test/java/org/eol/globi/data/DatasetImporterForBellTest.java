0253c0f8dec9fdd9a6a767dcebeff6b3a7e64c5a
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2026-08-08 21:34:26 -0500

    fix test error; https://github.com/globalbioticinteractions/globalbioticinteractions/issues/1184

--- eol-globi-neo4j-index/src/test/java/org/eol/globi/data/DatasetImporterForBellTest.java
+++ eol-globi-neo4j-index/src/test/java/org/eol/globi/data/DatasetImporterForBellTest.java
@@ -31,7 +31,7 @@ public class DatasetImporterForBellTest extends GraphDBTestCase {
             assertThat(study.getCitation(), startsWith("Bell, K. C., Matek, D., Demboski, J. R., & Cook, J. A. (2015). Expanded Host Range of Sucking Lice and Pinworms of Western North American Chipmunks. Comparative Parasitology, 82(2), 312–321. doi:10.1654/4756.1 . Data provided by Kayce C. Bell."));
         }
         try (Transaction transaction = getGraphDb().beginTx()) {
-            Result execute = transaction.execute("CYPHER 2.3 START taxon = node:taxons('*:*') RETURN taxon.name");
+            Result execute = transaction.execute("MATCH (taxon:Taxon_Verbatim) RETURN (taxon.name)");
             String actual = execute.resultAsString();
             assertThat(actual, CoreMatchers.containsString("Tamias"));
             assertThat(taxonIndex.findTaxonByName("Tamias speciosus"), is(notNullValue()));
