342dd112fb2d89e5786e13f6b13676474c7e5883
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2026-08-13 10:07:30 -0500

    fix test error; https://github.com/globalbioticinteractions/globalbioticinteractions/issues/1184

--- eol-globi-data-sources/src/test/java/org/eol/globi/data/DatasetImporterForFelderTest.java
+++ eol-globi-data-sources/src/test/java/org/eol/globi/data/DatasetImporterForFelderTest.java
@@ -20,7 +20,7 @@ import java.nio.charset.StandardCharsets;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-public class DatasetImporterFelderTest {
+public class DatasetImporterForFelderTest {
 
     public static final String[] FIELDS = new String[]{"Keyname",
             "Author",
