44a91f0a646152a40bc9003e6ad6d81a5779084f
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2026-08-18 02:27:52 -0500

    query rewrite; https://github.com/globalbioticinteractions/globalbioticinteractions/issues/1184

--- eol-globi-rest/src/test/java/org/eol/globi/util/CypherUtilIT.java
+++ eol-globi-rest/src/test/java/org/eol/globi/util/CypherUtilIT.java
@@ -5,6 +5,7 @@ import org.apache.commons.io.output.NullOutputStream;
 import org.apache.commons.io.output.ProxyOutputStream;
 import org.apache.http.HttpResponse;
 import org.eol.globi.server.util.RequestHelper;
+import org.eol.globi.server.util.ResultFormatterJSON;
 import org.hamcrest.core.Is;
 import org.junit.Test;
 
@@ -23,13 +24,15 @@ public class CypherUtilIT {
 
     @Test
     public void execute() throws IOException {
-        String query = "START study = node:studies('*:*') " +
-                "MATCH study-[:IN_DATASET]->dataset " +
-                "WHERE (has(study.externalId) AND study.externalId =~ {accordingTo}) OR (has(study.citation) AND study.citation =~ {accordingTo}) OR (has(dataset.citation) AND dataset.citation =~ {accordingTo}) " +
+        String query =
+                "MATCH (study:Reference)-[:IN_DATASET]->(dataset) " +
+                "WHERE (study.externalId IS NOT NULL AND study.externalId =~ $accordingTo) " +
+                        "OR (study.citation IS NOT NULL AND study.citation =~ $accordingTo) " +
+                        "OR (dataset.citation IS NOT NULL AND dataset.citation =~ $accordingTo) " +
                 "WITH study " +
-                "MATCH sourceTaxon<-[:CLASSIFIED_AS]-sourceSpecimen-[interaction:INTERACTS_WITH|PREYS_UPON|PARASITE_OF|HAS_HOST|HOST_OF|POLLINATES|PERCHING_ON|ATE|SYMBIONT_OF|PREYED_UPON_BY|POLLINATED_BY|EATEN_BY|HAS_PARASITE|PERCHED_ON_BY|HAS_PATHOGEN|PATHOGEN_OF|HAS_VECTOR|VECTOR_OF|VISITED_BY|VISITS|FLOWERS_VISITED_BY|VISITS_FLOWERS_OF|INHABITED_BY|INHABITS|ADJACENT_TO|CREATES_HABITAT_FOR|HAS_HABITAT|LIVED_ON_BY|LIVES_ON|LIVED_INSIDE_OF_BY|LIVES_INSIDE_OF|LIVED_NEAR_BY|LIVES_NEAR|LIVED_UNDER_BY|LIVES_UNDER|LIVES_WITH|ENDOPARASITE_OF|HAS_ENDOPARASITE|HYPERPARASITE_OF|HAS_HYPERPARASITE|HYPERPARASITOID_OF|HAS_HYPERPARASITOID|ECTOPARASITE_OF|HAS_ECTOPARASITE|KLEPTOPARASITE_OF|HAS_KLEPTOPARASITE|PARASITOID_OF|HAS_PARASITOID|ENDOPARASITOID_OF|HAS_ENDOPARASITOID|ECTOPARASITOID_OF|HAS_ECTOPARASITOID|GUEST_OF|HAS_GUEST_OF|FARMED_BY|FARMS|DAMAGED_BY|DAMAGES|DISPERSAL_VECTOR_OF|HAS_DISPERAL_VECTOR|KILLED_BY|KILLS|EPIPHITE_OF|HAS_EPIPHITE|LAYS_EGGS_ON|HAS_EGGS_LAYED_ON_BY|LAYS_EGGS_IN|HAS_EGGS_LAYED_IN_BY|COMMENSALIST_OF]->targetSpecimen-[:CLASSIFIED_AS]->targetTaxon, " +
-                "sourceSpecimen<-[collected_rel:COLLECTED]-study-[:IN_DATASET]->dataset " +
-                "OPTIONAL MATCH sourceSpecimen-[:COLLECTED_AT]->loc " +
+                "MATCH (sourceTaxon:Taxon)<-[:CLASSIFIED_AS]-(sourceSpecimen:Specimen)-[interaction:INTERACTS_WITH|PREYS_UPON|PARASITE_OF|HAS_HOST|HOST_OF|POLLINATES|PERCHING_ON|ATE|SYMBIONT_OF|PREYED_UPON_BY|POLLINATED_BY|EATEN_BY|HAS_PARASITE|PERCHED_ON_BY|HAS_PATHOGEN|PATHOGEN_OF|HAS_VECTOR|VECTOR_OF|VISITED_BY|VISITS|FLOWERS_VISITED_BY|VISITS_FLOWERS_OF|INHABITED_BY|INHABITS|ADJACENT_TO|CREATES_HABITAT_FOR|HAS_HABITAT|LIVED_ON_BY|LIVES_ON|LIVED_INSIDE_OF_BY|LIVES_INSIDE_OF|LIVED_NEAR_BY|LIVES_NEAR|LIVED_UNDER_BY|LIVES_UNDER|LIVES_WITH|ENDOPARASITE_OF|HAS_ENDOPARASITE|HYPERPARASITE_OF|HAS_HYPERPARASITE|HYPERPARASITOID_OF|HAS_HYPERPARASITOID|ECTOPARASITE_OF|HAS_ECTOPARASITE|KLEPTOPARASITE_OF|HAS_KLEPTOPARASITE|PARASITOID_OF|HAS_PARASITOID|ENDOPARASITOID_OF|HAS_ENDOPARASITOID|ECTOPARASITOID_OF|HAS_ECTOPARASITOID|GUEST_OF|HAS_GUEST_OF|FARMED_BY|FARMS|DAMAGED_BY|DAMAGES|DISPERSAL_VECTOR_OF|HAS_DISPERAL_VECTOR|KILLED_BY|KILLS|EPIPHITE_OF|HAS_EPIPHITE|LAYS_EGGS_ON|HAS_EGGS_LAYED_ON_BY|LAYS_EGGS_IN|HAS_EGGS_LAYED_IN_BY|COMMENSALIST_OF]->(targetSpecimen:Specimen)-[:CLASSIFIED_AS]->(targetTaxon:Taxon), " +
+                "(sourceSpecimen:Specimen)<-[collected_rel:COLLECTED]-(study:Reference)-[:IN_DATASET]->(dataset:Dataset) " +
+                "OPTIONAL MATCH (sourceSpecimen:Specimen)-[:COLLECTED_AT]->(loc:Location) " +
                 "RETURN sourceTaxon.externalId as source_taxon_external_id,sourceTaxon.name as source_taxon_name,sourceTaxon.path as source_taxon_path,sourceSpecimen.lifeStageLabel as source_specimen_life_stage,sourceSpecimen.basisOfRecordLabel as source_specimen_basis_of_record,interaction.label as interaction_type,targetTaxon.externalId as target_taxon_external_id,targetTaxon.name as target_taxon_name,targetTaxon.path as target_taxon_path,targetSpecimen.lifeStageLabel as target_specimen_life_stage,targetSpecimen.basisOfRecordLabel as target_specimen_basis_of_record,loc.latitude as latitude,loc.longitude as longitude,study.title as study_title";
         HashMap<String, String> params = new HashMap<String, String>() {{
             put("accordingTo", "(?i).*(\\\\Qgomexsi\\\\E).*");
@@ -45,24 +48,25 @@ public class CypherUtilIT {
                 }
             }
         };
-
         ByteArrayOutputStream os = new ByteArrayOutputStream();
         IOUtils.copy(execute.getEntity().getContent(), os);
         proxyOutputStream.flush();
         proxyOutputStream.close();
+
+        new ResultFormatterJSON().format(new String(os.toByteArray(), StandardCharsets.UTF_8));
     }
 
     @Test
     public void nonEmptyData() throws IOException {
-        String query = "START study = node:studies('*:*') " +
-                "MATCH study-[:IN_DATASET]->dataset " +
-                "WHERE (has(study.externalId) AND study.externalId =~ {accordingTo}) " +
-                "OR (has(study.citation) AND study.citation =~ {accordingTo}) " +
-                "OR (has(dataset.citation) AND dataset.citation =~ {accordingTo}) " +
+        String query =
+                "MATCH (study:Reference)-[:IN_DATASET]->(dataset:Dataset) " +
+                "WHERE (study.externalId IS NOT NULL AND study.externalId =~ $accordingTo) " +
+                "OR (study.citation IS NOT NULL AND study.citation =~ $accordingTo) " +
+                "OR (dataset.citation IS NOT NULL AND dataset.citation =~ $accordingTo) " +
                 "WITH study " +
-                "MATCH sourceTaxon<-[:CLASSIFIED_AS]-sourceSpecimen-[interaction:INTERACTS_WITH|PREYS_UPON|PARASITE_OF|HAS_HOST|HOST_OF|POLLINATES|PERCHING_ON|ATE|SYMBIONT_OF|PREYED_UPON_BY|POLLINATED_BY|EATEN_BY|HAS_PARASITE|PERCHED_ON_BY|HAS_PATHOGEN|PATHOGEN_OF|HAS_VECTOR|VECTOR_OF|VISITED_BY|VISITS|FLOWERS_VISITED_BY|VISITS_FLOWERS_OF|INHABITED_BY|INHABITS|ADJACENT_TO|CREATES_HABITAT_FOR|HAS_HABITAT|LIVED_ON_BY|LIVES_ON|LIVED_INSIDE_OF_BY|LIVES_INSIDE_OF|LIVED_NEAR_BY|LIVES_NEAR|LIVED_UNDER_BY|LIVES_UNDER|LIVES_WITH|ENDOPARASITE_OF|HAS_ENDOPARASITE|HYPERPARASITE_OF|HAS_HYPERPARASITE|HYPERPARASITOID_OF|HAS_HYPERPARASITOID|ECTOPARASITE_OF|HAS_ECTOPARASITE|KLEPTOPARASITE_OF|HAS_KLEPTOPARASITE|PARASITOID_OF|HAS_PARASITOID|ENDOPARASITOID_OF|HAS_ENDOPARASITOID|ECTOPARASITOID_OF|HAS_ECTOPARASITOID|GUEST_OF|HAS_GUEST_OF|FARMED_BY|FARMS|DAMAGED_BY|DAMAGES|DISPERSAL_VECTOR_OF|HAS_DISPERAL_VECTOR|KILLED_BY|KILLS|EPIPHITE_OF|HAS_EPIPHITE|LAYS_EGGS_ON|HAS_EGGS_LAYED_ON_BY|LAYS_EGGS_IN|HAS_EGGS_LAYED_IN_BY|COMMENSALIST_OF]->targetSpecimen-[:CLASSIFIED_AS]->targetTaxon, " +
-                "sourceSpecimen<-[collected_rel:COLLECTED]-study " +
-                "OPTIONAL MATCH sourceSpecimen-[:COLLECTED_AT]->loc " +
+                "MATCH (sourceTaxon:Taxon)<-[:CLASSIFIED_AS]-(sourceSpecimen:Specimen)-[interaction:INTERACTS_WITH|PREYS_UPON|PARASITE_OF|HAS_HOST|HOST_OF|POLLINATES|PERCHING_ON|ATE|SYMBIONT_OF|PREYED_UPON_BY|POLLINATED_BY|EATEN_BY|HAS_PARASITE|PERCHED_ON_BY|HAS_PATHOGEN|PATHOGEN_OF|HAS_VECTOR|VECTOR_OF|VISITED_BY|VISITS|FLOWERS_VISITED_BY|VISITS_FLOWERS_OF|INHABITED_BY|INHABITS|ADJACENT_TO|CREATES_HABITAT_FOR|HAS_HABITAT|LIVED_ON_BY|LIVES_ON|LIVED_INSIDE_OF_BY|LIVES_INSIDE_OF|LIVED_NEAR_BY|LIVES_NEAR|LIVED_UNDER_BY|LIVES_UNDER|LIVES_WITH|ENDOPARASITE_OF|HAS_ENDOPARASITE|HYPERPARASITE_OF|HAS_HYPERPARASITE|HYPERPARASITOID_OF|HAS_HYPERPARASITOID|ECTOPARASITE_OF|HAS_ECTOPARASITE|KLEPTOPARASITE_OF|HAS_KLEPTOPARASITE|PARASITOID_OF|HAS_PARASITOID|ENDOPARASITOID_OF|HAS_ENDOPARASITOID|ECTOPARASITOID_OF|HAS_ECTOPARASITOID|GUEST_OF|HAS_GUEST_OF|FARMED_BY|FARMS|DAMAGED_BY|DAMAGES|DISPERSAL_VECTOR_OF|HAS_DISPERAL_VECTOR|KILLED_BY|KILLS|EPIPHITE_OF|HAS_EPIPHITE|LAYS_EGGS_ON|HAS_EGGS_LAYED_ON_BY|LAYS_EGGS_IN|HAS_EGGS_LAYED_IN_BY|COMMENSALIST_OF]->(targetSpecimen:Specimen)-[:CLASSIFIED_AS]->(targetTaxon:Taxon), " +
+                "(sourceSpecimen:Specimen)<-[collected_rel:COLLECTED]-(study:Reference) " +
+                "OPTIONAL MATCH (sourceSpecimen:Specimen)-[:COLLECTED_AT]->(loc:Location) " +
                 "RETURN sourceTaxon.externalId as source_taxon_external_id,sourceTaxon.name as source_taxon_name,sourceTaxon.path as source_taxon_path,sourceSpecimen.lifeStageLabel as source_specimen_life_stage,sourceSpecimen.basisOfRecordLabel as source_specimen_basis_of_record,interaction.label as interaction_type,targetTaxon.externalId as target_taxon_external_id,targetTaxon.name as target_taxon_name,targetTaxon.path as target_taxon_path,targetSpecimen.lifeStageLabel as target_specimen_life_stage,targetSpecimen.basisOfRecordLabel as target_specimen_basis_of_record,loc.latitude as latitude,loc.longitude as longitude,study.title as study_title " +
                 "LIMIT 1";
         HashMap<String, String> params = new HashMap<String, String>() {{
@@ -71,21 +75,23 @@ public class CypherUtilIT {
         HttpResponse execute = CypherUtil.execute(new CypherQuery(query, params, CypherUtil.CYPHER_VERSION_5));
 
         String body = IOUtils.toString(execute.getEntity().getContent(), StandardCharsets.UTF_8);
+        new ResultFormatterJSON().format(body);
         assertFalse(RequestHelper.emptyData(body));
         assertThat(body, containsString("http://arctos.database.museum/guid/MSB:Para:1678"));
+
     }
 
     @Test
     public void emptyData() throws IOException {
-        String query = "START study = node:studies('*:*') " +
-                "MATCH study-[:IN_DATASET]->dataset " +
-                "WHERE (has(study.externalId) AND study.externalId =~ {accordingTo}) " +
-                "OR (has(study.citation) AND study.citation =~ {accordingTo}) " +
-                "OR (has(dataset.citation) AND dataset.citation =~ {accordingTo}) " +
+        String query =
+                "MATCH (study:Reference)-[:IN_DATASET]->(dataset:Dataset) " +
+                "WHERE (study.externalId IS NOT NULL AND study.externalId =~ $accordingTo) " +
+                "OR (study.citation IS NOT NULL AND study.citation =~ $accordingTo) " +
+                "OR (dataset.citation IS NOT NULL AND dataset.citation =~ $accordingTo) " +
                 "WITH study " +
-                "MATCH sourceTaxon<-[:CLASSIFIED_AS]-sourceSpecimen-[interaction:INTERACTS_WITH|PREYS_UPON|PARASITE_OF|HAS_HOST|HOST_OF|POLLINATES|PERCHING_ON|ATE|SYMBIONT_OF|PREYED_UPON_BY|POLLINATED_BY|EATEN_BY|HAS_PARASITE|PERCHED_ON_BY|HAS_PATHOGEN|PATHOGEN_OF|HAS_VECTOR|VECTOR_OF|VISITED_BY|VISITS|FLOWERS_VISITED_BY|VISITS_FLOWERS_OF|INHABITED_BY|INHABITS|ADJACENT_TO|CREATES_HABITAT_FOR|HAS_HABITAT|LIVED_ON_BY|LIVES_ON|LIVED_INSIDE_OF_BY|LIVES_INSIDE_OF|LIVED_NEAR_BY|LIVES_NEAR|LIVED_UNDER_BY|LIVES_UNDER|LIVES_WITH|ENDOPARASITE_OF|HAS_ENDOPARASITE|HYPERPARASITE_OF|HAS_HYPERPARASITE|HYPERPARASITOID_OF|HAS_HYPERPARASITOID|ECTOPARASITE_OF|HAS_ECTOPARASITE|KLEPTOPARASITE_OF|HAS_KLEPTOPARASITE|PARASITOID_OF|HAS_PARASITOID|ENDOPARASITOID_OF|HAS_ENDOPARASITOID|ECTOPARASITOID_OF|HAS_ECTOPARASITOID|GUEST_OF|HAS_GUEST_OF|FARMED_BY|FARMS|DAMAGED_BY|DAMAGES|DISPERSAL_VECTOR_OF|HAS_DISPERAL_VECTOR|KILLED_BY|KILLS|EPIPHITE_OF|HAS_EPIPHITE|LAYS_EGGS_ON|HAS_EGGS_LAYED_ON_BY|LAYS_EGGS_IN|HAS_EGGS_LAYED_IN_BY|COMMENSALIST_OF]->targetSpecimen-[:CLASSIFIED_AS]->targetTaxon, " +
-                "sourceSpecimen<-[collected_rel:COLLECTED]-study-[:IN_DATASET]->dataset " +
-                "OPTIONAL MATCH sourceSpecimen-[:COLLECTED_AT]->loc " +
+                "MATCH (sourceTaxon:Taxon)<-[:CLASSIFIED_AS]-(sourceSpecimen:Specimen)-[interaction:INTERACTS_WITH|PREYS_UPON|PARASITE_OF|HAS_HOST|HOST_OF|POLLINATES|PERCHING_ON|ATE|SYMBIONT_OF|PREYED_UPON_BY|POLLINATED_BY|EATEN_BY|HAS_PARASITE|PERCHED_ON_BY|HAS_PATHOGEN|PATHOGEN_OF|HAS_VECTOR|VECTOR_OF|VISITED_BY|VISITS|FLOWERS_VISITED_BY|VISITS_FLOWERS_OF|INHABITED_BY|INHABITS|ADJACENT_TO|CREATES_HABITAT_FOR|HAS_HABITAT|LIVED_ON_BY|LIVES_ON|LIVED_INSIDE_OF_BY|LIVES_INSIDE_OF|LIVED_NEAR_BY|LIVES_NEAR|LIVED_UNDER_BY|LIVES_UNDER|LIVES_WITH|ENDOPARASITE_OF|HAS_ENDOPARASITE|HYPERPARASITE_OF|HAS_HYPERPARASITE|HYPERPARASITOID_OF|HAS_HYPERPARASITOID|ECTOPARASITE_OF|HAS_ECTOPARASITE|KLEPTOPARASITE_OF|HAS_KLEPTOPARASITE|PARASITOID_OF|HAS_PARASITOID|ENDOPARASITOID_OF|HAS_ENDOPARASITOID|ECTOPARASITOID_OF|HAS_ECTOPARASITOID|GUEST_OF|HAS_GUEST_OF|FARMED_BY|FARMS|DAMAGED_BY|DAMAGES|DISPERSAL_VECTOR_OF|HAS_DISPERAL_VECTOR|KILLED_BY|KILLS|EPIPHITE_OF|HAS_EPIPHITE|LAYS_EGGS_ON|HAS_EGGS_LAYED_ON_BY|LAYS_EGGS_IN|HAS_EGGS_LAYED_IN_BY|COMMENSALIST_OF]->(targetSpecimen:Specimen)-[:CLASSIFIED_AS]->(targetTaxon:Taxon), " +
+                "(sourceSpecimen:Specimen)<-[collected_rel:COLLECTED]-(study:Reference)-[:IN_DATASET]->(dataset:Dataset) " +
+                "OPTIONAL MATCH (sourceSpecimen:Specimen)-[:COLLECTED_AT]->(loc:Location) " +
                 "RETURN sourceTaxon.externalId as source_taxon_external_id,sourceTaxon.name as source_taxon_name,sourceTaxon.path as source_taxon_path,sourceSpecimen.lifeStageLabel as source_specimen_life_stage,sourceSpecimen.basisOfRecordLabel as source_specimen_basis_of_record,interaction.label as interaction_type,targetTaxon.externalId as target_taxon_external_id,targetTaxon.name as target_taxon_name,targetTaxon.path as target_taxon_path,targetSpecimen.lifeStageLabel as target_specimen_life_stage,targetSpecimen.basisOfRecordLabel as target_specimen_basis_of_record,loc.latitude as latitude,loc.longitude as longitude,study.title as study_title " +
                 "LIMIT 1";
         HashMap<String, String> params = new HashMap<String, String>() {{
@@ -94,6 +100,7 @@ public class CypherUtilIT {
         HttpResponse execute = CypherUtil.execute(new CypherQuery(query, params, CypherUtil.CYPHER_VERSION_5));
 
         String body = IOUtils.toString(execute.getEntity().getContent(), StandardCharsets.UTF_8);
+        new ResultFormatterJSON().format(body);
         assertThat(RequestHelper.emptyData(body), Is.is(true));
     }
 
