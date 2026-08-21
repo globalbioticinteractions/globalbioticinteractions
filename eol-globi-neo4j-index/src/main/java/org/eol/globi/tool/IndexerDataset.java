f800a46b78df6666ddd1b90c2c78a5dddbe3a418
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2026-08-21 16:07:35 -0500

    create external id index; https://github.com/globalbioticinteractions/globalbioticinteractions/issues/1184

--- eol-globi-neo4j-index/src/main/java/org/eol/globi/tool/IndexerDataset.java
+++ eol-globi-neo4j-index/src/main/java/org/eol/globi/tool/IndexerDataset.java
@@ -9,6 +9,7 @@ import org.eol.globi.data.ParserFactoryLocal;
 import org.eol.globi.data.StudyImporterException;
 import org.eol.globi.db.GraphServiceFactory;
 import org.eol.globi.domain.LocationConstant;
+import org.eol.globi.domain.PropertyAndValueDictionary;
 import org.eol.globi.domain.StudyConstant;
 import org.eol.globi.service.DatasetLocal;
 import org.eol.globi.util.InputStreamFactoryNoop;
@@ -53,6 +54,7 @@ public class IndexerDataset implements IndexerNeo4j {
             Neo4jIndexUtil.createIndexIfNotExists(tx, NodeLabel.Reference, StudyConstant.TITLE_IN_NAMESPACE);
             Neo4jIndexUtil.createIndexIfNotExists(tx, NodeLabel.Location, LocationConstant.LOCALITY_ID);
             Neo4jIndexUtil.createIndexIfNotExists(tx, NodeLabel.Location, LocationConstant.LOCALITY);
+            Neo4jIndexUtil.createIndexIfNotExists(tx, NodeLabel.ExternalId, PropertyAndValueDictionary.EXTERNAL_ID);
             Neo4jIndexUtil.createPointLocationIndexIfNotExists(tx);
             tx.commit();
         }
