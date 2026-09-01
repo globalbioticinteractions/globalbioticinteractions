94d33ca91c6728a3308f89211752328171dc211d
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2026-09-01 15:24:36 -0500

    add shortcut for commonly used .csv path https://github.com/globalbioticinteractions/globalbioticinteractions/issues/1184

--- eol-globi-rest-services/src/main/java/org/eol/globi/server/InteractionController.java
+++ eol-globi-rest-services/src/main/java/org/eol/globi/server/InteractionController.java
@@ -1,5 +1,6 @@
 package org.eol.globi.server;
 
+import org.eol.globi.domain.PropertyAndValueDictionary;
 import org.eol.globi.server.util.RequestHelper;
 import org.eol.globi.util.CypherQuery;
 import org.eol.globi.util.CypherUtil;
@@ -16,6 +17,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 @Controller
 public class InteractionController {
@@ -47,8 +49,22 @@ public class InteractionController {
     @ResponseBody
     protected CypherQuery findInteractions(HttpServletRequest request) {
         Map parameterMap = getParamMap(request);
+        return findInteractions(request, parameterMap);
+    }
+
+    private static CypherQuery findInteractions(HttpServletRequest request, Map parameterMap) {
         CypherQuery query = CypherQueryBuilder.buildInteractionQuery(parameterMap, QueryType.forParams(parameterMap));
         return CypherQueryBuilder.createPagedQuery(request, query);
+    }
+
+    @RequestMapping(value = "/interaction.csv", method = {RequestMethod.GET})
+    @ResponseBody
+    protected CypherQuery findInteractionsAsCSV(HttpServletRequest request) {
+        TreeMap<String, String[]> modifiedParameters = new TreeMap<String, String[]>() {{
+            putAll(getParamMap(request));
+            put("type", new String[]{"csv"});
+        }};
+        return findInteractions(request, modifiedParameters);
     }
 
     private static Map getParamMap(HttpServletRequest request) {
